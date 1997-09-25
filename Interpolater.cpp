f0504e933eadf0799e11327feaf33eae735c8d84
almgren <almgren>
1997-09-25 04:46:30 +0000

    Changes to be up-to-date with latest additions and fixes in non-parallel amr library.

--- Interpolater.cpp
+++ Interpolater.cpp
@@ -14,8 +14,11 @@
 NodeBilinear     node_bilinear_interp;
 CellBilinear     cell_bilinear_interp;
 CellConservative cell_cons_interp;
+CellQuadratic    quadratic_interp;
 CellConservative unlimited_cc_interp(0);
 PCInterp         pc_interp;
+CellConservativeLinear lincc_interp;
+CellConservativeLinear nonlincc_interp(0);
 
 // ------------------------------------------------------------
 // --------  Virtual base class Interpolater  -----------------
@@ -246,7 +249,7 @@ CellConservative::interp(FARRAYBOX& crse, int crse_comp,
 
     t_long = cslope_bx.numPts();
     assert(t_long < INT_MAX);
-    int cslope_vol = cslope_bx.numPts();
+    int cslope_vol = int(t_long);
     int clo = 1 - loslp;
     int chi = clo + cslope_vol - 1;
     c_len = hislp - loslp + 1;
@@ -287,6 +290,304 @@ CellConservative::interp(FARRAYBOX& crse, int crse_comp,
     int slope_flag = (do_limited_slope ? 1 : 0);
 
     FORT_CCINTERP (fdat,ARLIM(flo),ARLIM(fhi),
+                   fblo[0], fblo[1],
+#if (BL_SPACEDIM == 3)
+                   fblo[2],
+#endif 
+                   fbhi[0], fbhi[1],
+#if (BL_SPACEDIM == 3)
+                   fbhi[2],
+#endif 
+                   &ncomp,ratio.getVect(),
+		   cdat,&clo,&chi,
+                   cblo[0], cblo[1],
+#if (BL_SPACEDIM == 3)
+                   cblo[2],
+#endif 
+                   cbhi[0], cbhi[1],
+#if (BL_SPACEDIM == 3)
+                   cbhi[2],
+#endif 
+                   fslo,fshi,
+		   cslope,&c_len,fslope,fstrip,&f_len,foff,
+		   (int*)bcr.dataPtr(), &slope_flag,
+                   D_DECL(fvc[0].dataPtr(),fvc[1].dataPtr(),fvc[2].dataPtr()),
+                   D_DECL(cvc[0].dataPtr(),cvc[1].dataPtr(),cvc[2].dataPtr())
+                   );
+}
+
+// -------------------------------------------------------------------
+// --------  Linear Conservative Cell Centered Interpolation  --------
+// -------------------------------------------------------------------
+//  The interpolation is linear in that it uses a limiting scheme that 
+//  preserves the value of any linear combination of the
+//  coarse grid data components--e.g., if 
+//  sum_ivar a(ic,jc,ivar)*fab(ic,jc,ivar) = 0, then
+//  sum_ivar a(ic,jc,ivar)*fab(if,jf,ivar) = 0 is satisfied
+//  in all fine cells if,jf covering coarse cell ic,jc.
+//
+//  If do_linear_limiting = 0, the interpolation scheme is identical to
+//  the used in ConservativeLinear for do_limited_slope=1; 
+//  the results should be exactly the same -- difference = hard 0.
+// -------------------------------------------------------------------
+
+CellConservativeLinear::CellConservativeLinear(int do_linear_limiting_)
+{
+    do_linear_limiting = do_linear_limiting_;
+}
+
+CellConservativeLinear::~CellConservativeLinear()
+{
+}
+
+// ------------------------------------------------------------
+BOX
+CellConservativeLinear::CoarseBox(const BOX& fine, const IntVect& ratio)
+{
+    BOX crse(::coarsen(fine,ratio));
+    crse.grow(1);
+    return crse;
+}
+
+// ------------------------------------------------------------
+BOX
+CellConservativeLinear::CoarseBox(const BOX& fine, int ratio)
+{
+    BOX crse(::coarsen(fine,ratio));
+    crse.grow(1);
+    return crse;
+}
+
+// ------------------------------------------------------------
+void
+CellConservativeLinear::interp(FARRAYBOX& crse, int crse_comp,
+			 FARRAYBOX& fine, int fine_comp,
+			 int ncomp,
+			 const BOX& fine_region, const IntVect & ratio,
+			 const Geometry& crse_geom,
+			 const Geometry& fine_geom,
+			 Array<BCRec>& bcr)
+{
+    assert( bcr.length() >= ncomp );
+    const BOX& domain = fine_geom.Domain();
+    if (!domain.contains(fine_region)) {
+	BoxLib::Error("interp:fine_region not in domain");
+    }
+
+    // make box which is intersection of fine_region and domain of fine
+    BOX target_fine_region = fine_region & fine.box();
+
+    // crse_bx is coarsening of target_fine_region, grown by 1
+    BOX crse_bx = CoarseBox(target_fine_region,ratio);
+
+    // slopes are needed only on coarsening of target_fine_region
+    BOX cslope_bx(crse_bx);
+    cslope_bx.grow(-1);
+
+      // get coarse in fine edge centered volume coordinates
+    Array<REAL> fvc[BL_SPACEDIM];
+    Array<REAL> cvc[BL_SPACEDIM];
+    int dir;
+    for (dir = 0; dir < BL_SPACEDIM; dir++) {
+        fine_geom.GetEdgeVolCoord(fvc[dir],target_fine_region,dir);
+        crse_geom.GetEdgeVolCoord(cvc[dir],crse_bx,dir);
+    }
+
+    // alloc tmp space for slope calc 
+
+    // In ucc_slopes and lcc_slopes , there is a slight abuse of 
+    // the number of compenents argument
+    // --> there is a slope for each component in each coordinate 
+    //     direction
+    FARRAYBOX ucc_slopes(cslope_bx,ncomp*BL_SPACEDIM);
+    FARRAYBOX lcc_slopes(cslope_bx,ncomp*BL_SPACEDIM);
+    FARRAYBOX slope_factors(cslope_bx,BL_SPACEDIM);
+
+    REAL* fdat       = fine.dataPtr(fine_comp);
+    const REAL* cdat = crse.dataPtr(crse_comp);
+    REAL* ucc_xsldat = ucc_slopes.dataPtr(0);
+    REAL* lcc_xsldat = lcc_slopes.dataPtr(0);
+    REAL* xslfac_dat = slope_factors.dataPtr(0);
+    REAL* ucc_ysldat = ucc_slopes.dataPtr(ncomp);
+    REAL* lcc_ysldat = lcc_slopes.dataPtr(ncomp);
+    REAL* yslfac_dat = slope_factors.dataPtr(1);
+#if (BL_SPACEDIM==3)
+    REAL* ucc_zsldat = ucc_slopes.dataPtr(2*ncomp);
+    REAL* lcc_zsldat = lcc_slopes.dataPtr(2*ncomp);
+    REAL* zslfac_dat = slope_factors.dataPtr(2);
+#endif
+    
+    const int* flo  = fine.loVect();
+    const int* fhi  = fine.hiVect();
+    const int* clo  = crse.loVect();
+    const int* chi  = crse.hiVect();
+    const int* fblo = target_fine_region.loVect();
+    const int* fbhi = target_fine_region.hiVect();
+    const int* csbhi = cslope_bx.hiVect();
+    const int* csblo = cslope_bx.loVect();
+
+    int lin_limit = (do_linear_limiting ? 1 : 0);
+
+    const int* cvcblo = crse_bx.loVect();
+    const int* fvcblo = target_fine_region.loVect();
+    int cvcbhi[BL_SPACEDIM];
+    int fvcbhi[BL_SPACEDIM];
+    for (dir=0; dir<BL_SPACEDIM; dir++) {
+      cvcbhi[dir] = cvcblo[dir] + cvc[dir].length() - 1;
+      fvcbhi[dir] = fvcblo[dir] + fvc[dir].length() - 1;
+    }
+
+    REAL *voffx = new REAL[fvc[0].length()];
+    REAL *voffy = new REAL[fvc[1].length()];
+#if (BL_SPACEDIM==3)
+    REAL *voffz = new REAL[fvc[2].length()];
+#endif
+
+    FORT_LINCCINTERP (fdat,ARLIM(flo),ARLIM(fhi),
+                      fblo, fbhi,
+                      ARLIM(fvcblo), ARLIM(fvcbhi),
+		      cdat,ARLIM(clo),ARLIM(chi),
+                      ARLIM(cvcblo), ARLIM(cvcbhi),
+                      ucc_xsldat, lcc_xsldat, xslfac_dat,
+                      ucc_ysldat, lcc_ysldat, yslfac_dat,
+#if (BL_SPACEDIM==3)
+                      ucc_zsldat, lcc_zsldat, zslfac_dat,
+#endif
+                      ARLIM(csblo), ARLIM(csbhi),
+                      csblo, csbhi,
+                      &ncomp,ratio.getVect(),
+		      (int*)bcr.dataPtr(), &lin_limit,
+                      D_DECL(fvc[0].dataPtr(),fvc[1].dataPtr(),fvc[2].dataPtr()),
+                      D_DECL(cvc[0].dataPtr(),cvc[1].dataPtr(),cvc[2].dataPtr()),
+                      D_DECL(voffx,voffy,voffz)
+                      );
+    delete voffx;
+    delete voffy;
+#if (BL_SPACEDIM==3)
+    delete voffz;
+#endif
+
+}
+
+// ------------------------------------------------------------
+// --------  Quadratic Cell-Centered Interpolation  --------
+// ------------------------------------------------------------
+
+CellQuadratic::CellQuadratic()
+{
+    strip = cslope = 0;
+    strip_len = slope_len = 0;
+}
+
+CellQuadratic::~CellQuadratic()
+{
+    delete strip;
+    delete cslope;
+}
+
+// ------------------------------------------------------------
+BOX
+CellQuadratic::CoarseBox(const BOX& fine, const IntVect& ratio)
+{
+    BOX crse(::coarsen(fine,ratio));
+    crse.grow(1);
+    return crse;
+}
+
+// ------------------------------------------------------------
+BOX
+CellQuadratic::CoarseBox(const BOX& fine, int ratio)
+{
+    BOX crse(::coarsen(fine,ratio));
+    crse.grow(1);
+    return crse;
+}
+
+// ------------------------------------------------------------
+void
+CellQuadratic::interp(FARRAYBOX& crse, int crse_comp,
+		      FARRAYBOX& fine, int fine_comp,
+		      int ncomp,
+		      const BOX& fine_region, const IntVect & ratio,
+		      const Geometry& crse_geom,
+		      const Geometry& fine_geom,
+		      Array<BCRec>& bcr)
+{
+
+    assert( bcr.length() >= ncomp );
+    const BOX& domain = fine_geom.Domain();
+    if (!domain.contains(fine_region)) {
+	BoxLib::Error("interp:fine_region not in domain");
+    }
+
+    // make box which is intersection of fine_region and domain of fine
+    BOX target_fine_region = fine_region & fine.box();
+
+    BOX crse_bx(::coarsen(target_fine_region,ratio));
+    BOX fslope_bx(::refine(crse_bx,ratio));
+    BOX cslope_bx(crse_bx);
+    cslope_bx.grow(1);
+    if (! crse.box().contains(cslope_bx) ) {
+	BoxLib::Error("CellQuadratic: crse databox size mismatch");
+    }
+
+      // alloc temp space for coarse grid slopes: here we use 5 
+      //  instead of BL_SPACEDIM because of the x^2, y^2 and xy terms
+    long t_long = cslope_bx.numPts();
+    assert(t_long < INT_MAX);
+    int c_len = int(t_long);
+    if (slope_len < 5*c_len) {
+	slope_len = 5*c_len;
+	delete cslope;
+	cslope = new REAL[slope_len];
+    }
+    int loslp    = cslope_bx.index(crse_bx.smallEnd());
+    int hislp    = cslope_bx.index(crse_bx.bigEnd());
+
+    t_long = cslope_bx.numPts();
+    assert(t_long < INT_MAX);
+    int cslope_vol = int(t_long);
+    int clo = 1 - loslp;
+    int chi = clo + cslope_vol - 1;
+    c_len = hislp - loslp + 1;
+
+      // alloc temp space for one strip of fine grid slopes: here we use 5 
+      //  instead of BL_SPACEDIM because of the x^2, y^2 and xy terms
+    int dir;
+    int f_len = fslope_bx.longside(dir);
+    if (strip_len < (5+2)*f_len) {
+	strip_len = (5+2)*f_len;
+	delete strip;
+	strip = new REAL[strip_len];
+    }
+    REAL *fstrip = strip;
+    REAL *foff = fstrip + f_len;
+    REAL *fslope = foff + f_len;
+
+      // get coarse in fine edge centered volume coordinates
+    Array<REAL> fvc[BL_SPACEDIM];
+    Array<REAL> cvc[BL_SPACEDIM];
+    for (dir = 0; dir < BL_SPACEDIM; dir++) {
+        fine_geom.GetEdgeVolCoord(fvc[dir],target_fine_region,dir);
+        crse_geom.GetEdgeVolCoord(cvc[dir],crse_bx,dir);
+    }
+
+      // alloc tmp space for slope calc and to allow for vectorization
+    REAL* fdat      = fine.dataPtr(fine_comp);
+    const REAL* cdat= crse.dataPtr(crse_comp);
+    const int* flo  = fine.loVect();
+    const int* fhi  = fine.hiVect();
+    const int* fblo = target_fine_region.loVect();
+    const int* fbhi = target_fine_region.hiVect();
+    const int* cblo = crse_bx.loVect();
+    const int* cbhi = crse_bx.hiVect();
+    const int* cflo = crse.loVect();
+    const int* cfhi = crse.hiVect();
+    const int* fslo = fslope_bx.loVect();
+    const int* fshi = fslope_bx.hiVect();
+    int slope_flag = (do_limited_slope ? 1 : 0);
+
+    FORT_CQINTERP (fdat,ARLIM(flo),ARLIM(fhi),
                    fblo[0], fblo[1],
 #if (BL_SPACEDIM == 3)
                    fblo[2],
