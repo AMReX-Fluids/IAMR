1dfefc5de816d70970d65229361d01b79c7f593b
lijewski <lijewski>
1998-05-29 23:20:30 +0000

    Some cleanup.

--- ViscBndry.cpp
+++ ViscBndry.cpp
@@ -1,50 +1,62 @@
 //
-// $Id: ViscBndry.cpp,v 1.9 1998-05-08 21:55:03 marc Exp $
+// $Id: ViscBndry.cpp,v 1.10 1998-05-29 23:20:30 lijewski Exp $
 //
 
 #include <LO_BCTYPES.H>
 #include <ViscBndry.H>
 
-void ViscBndry::setBndryConds(const BCRec& bc, IntVect& ratio)
+void
+ViscBndry::setBndryConds (const BCRec&   bc,
+                          /*const*/ IntVect& ratio)
 {
-
-//  NOTE: ALL BCLOC VALUES ARE NOW DEFINED AS A LENGTH IN PHYSICAL DIMENSIONS
-//        *RELATIVE* TO THE FACE, NOT IN ABSOLUTE PHYSICAL SPACE
-
-    const BoxArray& grids = boxes();
-    int ngrds = grids.length();
-    const Real* dx = geom.CellSize();
+    //
+    //  NOTE: ALL BCLOC VALUES ARE NOW DEFINED AS A LENGTH IN PHYSICAL
+    //        DIMENSIONS *RELATIVE* TO THE FACE, NOT IN ABSOLUTE PHYSICAL SPACE
+    //
+    const Real* dx    = geom.CellSize();
     const Box& domain = geom.Domain();
-    const RealBox& prob_domain = geom.ProbDomain();
 
     int comp = 0;
-    for (OrientationIter fi; fi; ++fi) {
-        Orientation face(fi());
-        Array<Real> &bloc = bcloc[face];
-        Array<Array<BoundCond> >& bctag = bcond[face];
 
-        int dir = face.coordDir();
-        Real delta = dx[dir]*ratio[dir];
-        int p_bc = (face.isLow() ? bc.lo(dir) : bc.hi(dir));
+    for (OrientationIter fi; fi; ++fi)
+    {
+        Array<Real>& bloc = bcloc[fi()];
+        Array< Array<BoundCond> >& bctag = bcond[fi()];
 
-        for (int i = 0; i < ngrds; i++) {
-            const Box& grd = grids[i];
+        int dir          = fi().coordDir();
+        const Real delta = dx[dir]*ratio[dir];
+        int p_bc         = (fi().isLow() ? bc.lo(dir) : bc.hi(dir));
 
-            if (domain[face] == grd[face] && !geom.isPeriodic(dir)) {
-                  // All physical bc values are located on face
-                if (p_bc == EXT_DIR) {
+        for (int i = 0; i < boxes().length(); i++)
+        {
+            if (domain[fi()] == boxes()[i][fi()] && !geom.isPeriodic(dir))
+            {
+                //
+                // All physical bc values are located on face.
+                //
+                if (p_bc == EXT_DIR)
+                {
                     bctag[i][comp] = LO_DIRICHLET;
                     bloc[i] = 0.;
-                } else if (p_bc == EXTRAP || p_bc == HOEXTRAP || 
-                           p_bc == REFLECT_EVEN) {
+                }
+                else if (p_bc == EXTRAP        ||
+                         p_bc == HOEXTRAP      || 
+                         p_bc == REFLECT_EVEN)
+                {
                     bctag[i][comp] = LO_NEUMANN;
                     bloc[i] = 0.;
-                } else if (p_bc == REFLECT_ODD) {
+                }
+                else if (p_bc == REFLECT_ODD)
+                {
                     bctag[i][comp] = LO_REFLECT_ODD;
                     bloc[i] = 0.;
                 }
-            } else {
-                  // internal bndry
+            }
+            else
+            {
+                //
+                // Internal bndry.
+                //
                 bctag[i][comp] = LO_DIRICHLET;
                 bloc[i] = 0.5*delta;
             }
@@ -52,29 +64,17 @@ void ViscBndry::setBndryConds(const BCRec& bc, IntVect& ratio)
     }
 }
 
-// *************************************************************************
-
 void
-ViscBndry::setHomogValues(const BCRec& bc, IntVect& ratio)
+ViscBndry::setHomogValues (const BCRec& bc,
+                           /*const*/ IntVect& ratio)
 {
-
     setBndryConds(bc, ratio);
 
-/*
-    int ngrd = grids.length();
-    for (int grd = 0; grd < ngrd; grd++) {
-        const Box& bx = grids[grd];
-        for (OrientationIter fi; fi; ++fi) {
-            Orientation face(fi());
-            FArrayBox& bnd_fab = bndry[face][grd];
-            bnd_fab.setVal(0.);
+    for (OrientationIter fi; fi; ++fi)
+    {
+        for (FabSetIterator fsi(bndry[fi()]); fsi.isValid(); ++fsi)
+        {
+            fsi().setVal(0.);
         }
-    }
-*/
-    for(OrientationIter fi; fi; ++fi) {
-      Orientation face(fi());
-      for(FabSetIterator fsi(bndry[face]); fsi.isValid(); ++fsi) {
-        fsi().setVal(0.);
-      }
     }
 }
