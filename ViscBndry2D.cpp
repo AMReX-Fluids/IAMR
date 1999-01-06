18d1dd9bed3595a590c5df4d2de74d4c9b9e0533
marc <marc>
1999-01-06 00:51:15 +0000

    Bring tensor guys into line with newer tensor classes...just to make look like mglib stuff really...nothing new here.

--- ViscBndry2D.cpp
+++ ViscBndry2D.cpp
@@ -1,85 +1,66 @@
 #if (BL_SPACEDIM==2) && defined (USE_TENSOR)
 
 //
-// $Id: ViscBndry2D.cpp,v 1.12 1998-11-18 17:48:29 lijewski Exp $
+// $Id: ViscBndry2D.cpp,v 1.13 1999-01-06 00:51:15 marc Exp $
 //
 
 #include <LO_BCTYPES.H>
 #include <ViscBndry2D.H>
 
 void
-ViscBndry2D::setBndryConds (const Array<BCRec>& bcarray,
-                            const Geometry&     geom,
-                            int                 ratio)
+ViscBndry2D::setBndryConds (const BCRec& bc,
+                            int          ratio,
+                            int          comp)
 {
-    int ncomp = bcond[0][0].length();
+#if BL_SPACEDIM == 2
+    assert(comp<2*2); // u and v, plus derivs of same
+#elif BL_SPACEDIM == 3
+    assert(comp<3*(3+1)); // u and v, plus derivs of same
+#endif
 
-    assert(ncomp == 2*2); // u and v, plus derivs of same.
-    assert(bcarray.length() == 2*2);
-
-    const BoxArray& grids      = boxes();
-    const int ngrds            = grids.length();
-    const Real* dx             = geom.CellSize();
-    const Box& domain          = geom.Domain();
-    const RealBox& prob_domain = geom.ProbDomain();
+    const REAL* dx = geom.CellSize();
+    const BOX& domain = geom.Domain();
 
     for (OrientationIter fi; fi; ++fi)
     {
-	const Orientation face(fi());
+        Array<REAL> &bloc = bcloc[fi()];
+        Array< Array<BoundCond> >& bctag = bcond[fi()];
 
-	Array<Real>& bloc = bcloc[face];
-
-	int dir = face.coordDir();
+        int dir = fi().coordDir();
 #if 0
 	Real delta = dx[dir]*ratio[dir];
 #else
 	Real delta = dx[dir]*ratio;
 #endif
-	for (int icomp = 0; icomp < ncomp; icomp++)
+        int p_bc = (fi().isLow() ? bc.lo(dir): bc.hi(dir));
+
+        for (int i = 0; i < boxes().length(); i++)
         {
-            int p_bc = face.isLow() ? 
-                        bcarray[icomp].lo(dir) : bcarray[icomp].hi(dir);
-
-            for (int i = 0; i < ngrds; i++)
+            if (domain[fi()] == boxes()[i][fi()] && !geom.isPeriodic(dir))
             {
-                const Box& grd = grids[i];
-                //
-                // bctag is bc type (with array info on orientation,grid,comp)gone
-                //
-                BoundCond& bctag = bcond[face][i][icomp];
-
-                if (domain[face] == grd[face] && !geom.isPeriodic(dir))
+                // All physical bc values are located on face
+                if (p_bc == EXT_DIR ) {
+                    bctag[i][comp] = LO_DIRICHLET;
+                    bloc[i] = 0.0;
+                } else if (p_bc == FOEXTRAP      ||
+                           p_bc == HOEXTRAP      || 
+                           p_bc == REFLECT_EVEN)
                 {
-                    //
-                    // All physical bc values are located on face.
-                    //
-                    if (p_bc == EXT_DIR )
-                    {
-                        bctag = LO_DIRICHLET;
-                        bloc[i] = 0.0; // on face, distance to face = 0
-                    }
-                    else if (p_bc == FOEXTRAP || p_bc == HOEXTRAP || 
-                             p_bc == REFLECT_EVEN)
-                    {
-                        bctag = LO_NEUMANN;
-                        bloc[i] = 0.0; // on face, distance to face = 0
-                    }
-                    else if (p_bc == REFLECT_ODD)
-                    {
-                        bctag = LO_REFLECT_ODD;
-                        bloc[i] = 0.0; // on face, distance to face = 0
-                    }
-                }
-                else
+                    bctag[i][comp] = LO_NEUMANN;
+                    bloc[i] = 0.0;
+                } else if( p_bc == REFLECT_ODD )
                 {
-                    //
-                    // internal bndry.
-                    //
-                    bctag = LO_DIRICHLET;
-                    bloc[i] = 0.5*delta; // internal, distance is half of crse
+                    bctag[i][comp] = LO_REFLECT_ODD;
+                    bloc[i] = 0.0;
                 }
             }
-	}
+            else
+            {
+                // internal bndry, distance is half of crse
+                bctag[i][comp] = LO_DIRICHLET;
+                bloc[i] = 0.5*delta;
+            }
+        }
     }
 }
 
@@ -87,17 +68,12 @@ void
 ViscBndry2D::setHomogValues (const Array<BCRec>& bc,
                              int                 ratio)
 {
-    setBndryConds(bc, geom, ratio);
-
-    const int ngrd = grids.length();
-
-    for (int grd = 0; grd < ngrd; grd++)
-    {
+    for (int n=0; n<bc.length(); ++n)
+        setBndryConds(bc[n], ratio, n);
+    
+    for (int grd = 0; grd < boxes().length(); grd++)
         for (OrientationIter fi; fi; ++fi)
-        {
-            bndry[fi()][grd].setVal(0);
-        }
-    }
+            bndry[fi()][grd].setVal(0.);
 }
 
 #endif /*(BL_SPACEDIM==2) && defined (USE_TENSOR)*/
