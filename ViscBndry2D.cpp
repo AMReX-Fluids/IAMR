e19f5e4e1cc227e6f35979046f71c5c6c8c9442f
lijewski <lijewski>
1999-02-03 17:59:36 +0000

    Hadn't been parallelized.

--- ViscBndry2D.cpp
+++ ViscBndry2D.cpp
@@ -1,7 +1,7 @@
 #if (BL_SPACEDIM==2) && defined (USE_TENSOR)
 
 //
-// $Id: ViscBndry2D.cpp,v 1.13 1999-01-06 00:51:15 marc Exp $
+// $Id: ViscBndry2D.cpp,v 1.14 1999-02-03 17:59:36 lijewski Exp $
 //
 
 #include <LO_BCTYPES.H>
@@ -13,17 +13,17 @@ ViscBndry2D::setBndryConds (const BCRec& bc,
                             int          comp)
 {
 #if BL_SPACEDIM == 2
-    assert(comp<2*2); // u and v, plus derivs of same
+    assert(comp < 2*2);     // u and v, plus derivs of same
 #elif BL_SPACEDIM == 3
-    assert(comp<3*(3+1)); // u and v, plus derivs of same
+    assert(comp < 3*(3+1)); // u and v, plus derivs of same
 #endif
 
-    const REAL* dx = geom.CellSize();
-    const BOX& domain = geom.Domain();
+    const REAL* dx     = geom.CellSize();
+    const BOX&  domain = geom.Domain();
 
     for (OrientationIter fi; fi; ++fi)
     {
-        Array<REAL> &bloc = bcloc[fi()];
+        Array<REAL>&               bloc  = bcloc[fi()];
         Array< Array<BoundCond> >& bctag = bcond[fi()];
 
         int dir = fi().coordDir();
@@ -32,23 +32,26 @@ ViscBndry2D::setBndryConds (const BCRec& bc,
 #else
 	Real delta = dx[dir]*ratio;
 #endif
-        int p_bc = (fi().isLow() ? bc.lo(dir): bc.hi(dir));
+        int p_bc = fi().isLow() ? bc.lo(dir): bc.hi(dir);
 
         for (int i = 0; i < boxes().length(); i++)
         {
             if (domain[fi()] == boxes()[i][fi()] && !geom.isPeriodic(dir))
             {
-                // All physical bc values are located on face
-                if (p_bc == EXT_DIR ) {
+                //
+                // All physical bc values are located on face.
+                //
+                if (p_bc == EXT_DIR )
+                {
                     bctag[i][comp] = LO_DIRICHLET;
                     bloc[i] = 0.0;
-                } else if (p_bc == FOEXTRAP      ||
-                           p_bc == HOEXTRAP      || 
-                           p_bc == REFLECT_EVEN)
+                }
+                else if (p_bc == FOEXTRAP || p_bc == HOEXTRAP || p_bc == REFLECT_EVEN)
                 {
                     bctag[i][comp] = LO_NEUMANN;
                     bloc[i] = 0.0;
-                } else if( p_bc == REFLECT_ODD )
+                }
+                else if (p_bc == REFLECT_ODD)
                 {
                     bctag[i][comp] = LO_REFLECT_ODD;
                     bloc[i] = 0.0;
@@ -56,7 +59,9 @@ ViscBndry2D::setBndryConds (const BCRec& bc,
             }
             else
             {
-                // internal bndry, distance is half of crse
+                //
+                // Internal bndry, distance is half of crse.
+                //
                 bctag[i][comp] = LO_DIRICHLET;
                 bloc[i] = 0.5*delta;
             }
@@ -68,12 +73,16 @@ void
 ViscBndry2D::setHomogValues (const Array<BCRec>& bc,
                              int                 ratio)
 {
-    for (int n=0; n<bc.length(); ++n)
+    for (int n = 0; n < bc.length(); ++n)
         setBndryConds(bc[n], ratio, n);
-    
-    for (int grd = 0; grd < boxes().length(); grd++)
-        for (OrientationIter fi; fi; ++fi)
-            bndry[fi()][grd].setVal(0.);
+
+    for (OrientationIter fi; fi; ++fi)
+    {
+        for (FabSetIterator fsi(bndry[fi()]); fsi.isValid(); ++fsi)
+        {
+            fsi().setVal(0);
+        }
+    }
 }
 
 #endif /*(BL_SPACEDIM==2) && defined (USE_TENSOR)*/
