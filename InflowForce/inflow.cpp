e4d868fe01a08cb4a3fe6b9225f84c5270476c83
lijewski <lijewski>
2007-01-20 18:32:45 +0000

    first cut at using new style turb file

---
+++ InflowForce/inflow.cpp
@@ -0,0 +1,94 @@
+
+#include <cstdlib>
+#include <iostream>
+#include <fstream>
+#include <string>
+
+#include "REAL.H"
+#include "Utility.H"
+#include "FArrayBox.H"
+#include "ParallelDescriptor.H"
+
+#  if defined(BL_FORT_USE_UPPERCASE)
+#    define FORT_GETPLANE    GETPLANE
+#  elif defined(BL_FORT_USE_LOWERCASE)
+#    define FORT_GETPLANE    getplane
+#  elif defined(BL_FORT_USE_UNDERSCORE)
+#    define FORT_GETPLANE    getplane_
+#  endif
+
+extern "C" void FORT_GETPLANE(int* filename, int* len, Real* data, int* plane, int* ncomp);
+
+void
+FORT_GETPLANE (int* filename, int* len, Real* data, int* plane, int* ncomp)
+{
+    static int         kmax;
+    static bool        first = true;
+    static Array<long> offset;
+    std::string        flctfile;
+
+    for (int i = 0; i < *len; i++)
+    {
+        char c = filename[i];
+
+        flctfile += c;
+    }
+
+    if (first)
+    {
+        //
+        // Read and save all the seekp() offsets in the inflow header file.
+        //
+        first = false;
+
+        std::string hdr = flctfile; hdr += "/HDR";
+
+        std::ifstream ifs;
+
+        ifs.open(hdr.c_str(), std::ios::in);
+
+        if (!ifs.good())
+            BoxLib::FileOpenFailed(hdr);
+
+        int  idummy;
+        Real rdummy;
+
+        for (int i = 0; i < BL_SPACEDIM; i++)
+            ifs >> kmax;
+        ifs >> rdummy >> rdummy >> rdummy;
+        ifs >> idummy >> idummy >> idummy;
+
+        offset.resize(kmax * BL_SPACEDIM);
+
+        for (int i = 0; i < offset.size(); i++)
+            ifs >> offset[i];
+    }
+
+    std::string dat = flctfile; dat += "/DAT";
+
+    std::ifstream ifs;
+
+    ifs.open(dat.c_str(), std::ios::in);
+
+    if (!ifs.good())
+        BoxLib::FileOpenFailed(dat);
+    //
+    // There are BL_SPACEDIM * kmax planes of FABs.
+    // The first component are in the first kmax planes,
+    // the second component in the next kmax planes, ....
+    // Note also that both (*plane) and (*ncomp) start from
+    // 1 not 0 since they're passed from Fortran.
+    //
+    const long start = offset[((*plane) - 1) + (((*ncomp) - 1) * kmax)] ;
+
+    ifs.seekg(start, std::ios::beg);
+
+    if (!ifs.good())
+        BoxLib::Abort("getplane(): seekg() failed");
+
+    FArrayBox fab;
+
+    fab.readFrom(ifs);
+
+    memcpy(data, fab.dataPtr(), fab.box().numPts()*sizeof(Real));
+}
