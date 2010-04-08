54fb33eb1bb64ac60c5c8b5d39ad254bee00deab
lijewski <lijewski>
2010-04-08 17:44:58 +0000

    work getting turbMerge to work again

--- InflowForce/main.cpp
+++ InflowForce/main.cpp
@@ -1,64 +1,87 @@
 //
 // Merge swirl-type turbulence files together over specified range.
 //
-
-#include <cstdlib>
-#include <iostream>
+// Input file format:
+//
+//   N
+//   time1 fabname1
+//   time2 fabname2
+//   time3 fabname3
+//   time4 fabname4
+//   ...
+//   ...
+//
+// For a total of N lines.
+//
 
 #include <list>
-#include <REAL.H>
+#include <limits>
 #include <vector>
+#include <cstdlib>
+#include <iostream>
+#include <fstream>
+
+#include <REAL.H>
 #include <BLassert.H>
 #include <BoxLib.H>
+#include <FArrayBox.H>
 #include <ParmParse.H>
+#include <Utility.H>
 
-#include "main_F.H"
+struct Line
+{
+    Line (Real               time,
+          const std::string& name)
+        :
+        m_time(time),
+        m_name(name)
+        {}
+
+    bool operator<  (const Line& rhs) const { return m_time < rhs.m_time;  }
+
+    bool operator== (const Line& rhs) const { return m_time == rhs.m_time; }
+
+    Real        m_time;
+    std::string m_name;
+};
 
 static
 void
 usage ()
 {
-    std::cout << "usage: turbMerge"
-              << " range=\"tstart tend\""
-              << " ifiles=\"ifile1 ... ifileN\""
-              << " ofile=ofname"
+    std::cout << "\nusage: turbMerge"
+              << " ifile=inputfile"
+              << " ofile=outputfile"
+              << " DX=\"dx dy dz\"]\n"
+              << " [range=\"tstart tend\"]\n"
               << std::endl;
     exit(1);
 }
 
 static
-std::vector<int>
-EncodeStringForFortran(const std::string& astr)
+void
+GetFab (const std::string& name, FArrayBox& fab)
 {
-    int length = astr.size();
-    std::vector<int> result(length);
-    for (int i = 0; i < length; ++i)
-        result[i] = astr[i];
-    return result;
+    std::ifstream ifs(name.c_str(), std::ios::in);
+
+    std::cout << "Attempting to read FAB from file: " << name << " ... " << std::flush;
+
+    fab.readFrom(ifs);
+
+    std::cout << "done\n\n" << std::flush;
+
+    if (fab.box().length(2) != 1)
+    {
+        std::cout << "Z dimension of FAB box is not one!\n";
+        usage();
+    }
+
+    if (fab.nComp() < 3)
+    {
+        std::cout << "FAB has fewer than three components!\n";
+        usage();
+    }
 }
-
-struct Triplet
-{
-    Triplet ()
-        :
-        first(0), second(0), third(0) {}
-
-    Triplet (Real r ,int i, int j)
-        :
-        first(r), second(i), third(j) {}
-
-    bool operator< (const Triplet& rhs) const { return first < rhs.first; }
-
-    bool operator== (const Triplet& rhs) const { return first == rhs.first; }
-
-    Real first;  // Time corresponding to the platter of data.
-    int  second; // Index of the file containing that platter of data.
-    int  third;  // Fortran index in that file of the platter of data.
-};
-
-typedef Triplet Platter;
-
-typedef std::list<Platter> PList;
 
 int
 main (int   argc,
@@ -68,197 +91,205 @@ main (int   argc,
 
     ParmParse pp;
 
-    std::string              ofile;
-    std::vector<Real>        range;
-    std::vector<std::string> ifiles;
+    std::string       ofile;
+    std::vector<Real> range;
+    std::vector<Real> DX;
+    std::string       ifile;
 
-    pp.queryarr("range",range);
+    pp.queryarr("range", range);
 
-    if (range.size() != 2) usage();
+    if (range.empty())
+    {
+        //
+        // Use full range of times in the input file.
+        //
+        range.push_back(-1);
+        range.push_back( std::numeric_limits<Real>::max());
+    }
+    else
+    {
+        if (range.size() != 2) usage();
+    }
 
     if (range[0] >= range[1])
     {
-        std::cout << "range[0] MUST be less than range[1]!!!" << std::endl;
+        std::cout << "range[0] MUST be less than range[1]!!!\n";
         usage();
     }
 
-    switch(pp.countval("ifiles"))
+    std::cout << "\nrange: " << range[0] << ' ' << range[1] << '\n';
+
+    pp.queryarr("DX", DX);
+
+    if (DX.size() != 3)
     {
-    case 0:  usage();                                      break;
-    case 1:  ifiles.resize(1); pp.get("ifiles",ifiles[0]); break;
-    default: pp.queryarr("ifiles",ifiles);                 break;
+        std::cout << "DX must have three components!\n";
+        usage();
     }
+
+    std::cout << "\nDX: " << DX[0] << ' ' << DX[1] << ' ' << DX[2] << '\n';
+
+    pp.query("ifile",ifile);
+
+    if (ifile.empty()) usage();
 
     pp.query("ofile",ofile);
 
     if (ofile.empty()) usage();
 
-    const int NN = 1024;
-    int  imax, jmax, kmax, ncomp;
-    Real scalex, scaley, scalez;
-    std::vector<Real> times(NN);
-    std::vector< std::vector<Real> > ftimes(ifiles.size());
+    std::cout << "\nifile: " << ifile << '\n';
+    std::cout << "\nofile: " << ofile << '\n';
 
-    std::vector<int> fname = EncodeStringForFortran(ifiles[0]);
-    int              flen  = fname.size();
+    std::ifstream ifs(ifile.c_str(), std::ios::in);
 
-    std::cout << "Attempting to read header from: " << ifiles[0] << std::endl;
-    FORT_READ_SWIRL_HEADER(&fname[0],&flen,&imax,&jmax,&kmax,&ncomp,
-                           &scalex,&scaley,&scalez,&times[0],&NN);
+    int N;
 
-    ftimes[0].resize(kmax);
-    for (int i = 0; i < kmax; i++)
-        ftimes[0][i] = times[i];
+    ifs >> N;
 
-    std::cout << "imax, jmax, ncomp: "
-              << imax << ", "
-              << jmax << ", "
-              << ncomp << std::endl;
+    std::cout << "\nN: " << N << std::endl;
 
-    std::cout << "scalex, scaley, scalez: "
-              << scalex << ", "
-              << scaley << ", "
-              << scalez << std::endl;
-
-    for (int i = 1; i < ifiles.size(); i++)
+    if (N  < 2)
     {
-        const int NN = 1024;
-        int  imaxt, jmaxt, kmaxt, ncompt;
-        Real scalext, scaleyt, scalezt;
-
-        std::vector<int> fname = EncodeStringForFortran(ifiles[i]);
-        int              flen  = fname.size();
-
-        std::cout << "Attempting to read header from: " << ifiles[i] << std::endl;
-
-        FORT_READ_SWIRL_HEADER(&fname[0],&flen,&imaxt,&jmaxt,&kmaxt,&ncompt,
-                               &scalext,&scaleyt,&scalezt,&times[0],&NN);
-
-        ftimes[i].resize(kmaxt);
-        for (int j = 0; j < kmaxt; j++)
-            ftimes[i][j] = times[j];
-
-        if (imax != imaxt) {
-            std::cout << "imax != imaxt" << std::endl; exit(1);
-        }
-        if (jmax != jmaxt) {
-            std::cout << "jmax != jmaxt" << std::endl; exit(1);
-        }
-        if (scalex != scalext) {
-            std::cout << "scalex != scalext" << std::endl; exit(1);
-        }
-        if (scaley != scaleyt) {
-            std::cout << "scaley != scaleyt" << std::endl; exit(1);
-        }
+        std::cout << "ifile needs to contain at least two lines!\n";
+        usage();
     }
 
-    PList LP;
+    std::list<Line> LL;
 
-    for (int i = 0; i < ifiles.size(); i++)
-        for (int j = 0; j < ftimes[i].size(); j++)
-            if (ftimes[i][j] >= range[0] && ftimes[i][j] <= range[1])
-                LP.push_back(Platter(ftimes[i][j],i,j+1));
+    Real        tm;
+    std::string nm;
 
-    LP.sort();
-
-    LP.unique();
-
-    if (LP.empty())
+    for (int i = 0; i < N; i++)
     {
-        std::cout << "No data falls within the specified range!" << std::endl;
-        exit(1);
-    }
+        ifs >> tm;
+        ifs >> nm;
 
-    std::cout << ofile
-              << " will have "
-              << LP.size()
-              << " platters of data!" << std::endl;
-
-    std::cout << "Time       File      Index" << std::endl;
-    std::cout << "--------------------------" << std::endl;
-
-    for (PList::const_iterator it = LP.begin(); it != LP.end(); ++it)
-    {
-        std::cout << it->first
-                  << "\t"
-                  << ifiles[it->second]
-                  << "\t"
-                  << it->third
-                  << std::endl;
-    }
-
-    kmax = LP.size();
-
-    Real* fluct = new Real[kmax];
-
-    Real* block = new Real[imax*jmax*kmax*ncomp];
-
-    int putidx = 1;
-
-    for (PList::const_iterator it = LP.begin(); it != LP.end(); )
-    {
-        std::vector<int> fname  = EncodeStringForFortran(ifiles[it->second]);
-        int              flen   = fname.size();
-        int              getidx = it->third;
-        int              count  = 1;  // How many consecutive platters?
-
-        fluct[putidx-1] = it->first;
-
-        PList::const_iterator cit = it; ++cit;
-
-        for ( ; cit != LP.end(); ++cit)
+        if (tm < 0)
         {
-            if (ifiles[it->second] == ifiles[cit->second])
-            {
-                BL_ASSERT((it->third + count) == cit->third);
-
-                fluct[putidx+count-1] = cit->first;
-
-                count++;
-            }
-            else
-                break;
+            std::cout << "Got a negative time!\n";
+            usage();
         }
 
-        std::cout << "Reading "
-                  << count
-                  << " platters starting at "
-                  << getidx
-                  << " from "
-                  << ifiles[it->second] << std::endl;
+        if (nm.empty())
+        {
+            std::cout << "Got an empty FAB file name!\n";
+            usage();
+        }
 
-        FORT_FILL(block,
-                  &imax,
-                  &jmax,
-                  &kmax,
-                  &ncomp,
-                  &fluct[putidx-1],
-                  &putidx,
-                  &getidx,
-                  &fname[0],
-                  &flen,
-                  &count);
-
-        putidx += count;
-        for (int i = 0; i < count; i++) ++it;
+        LL.push_back(Line(tm,nm));
     }
 
-    std::cout << "Building swirl-type turbulence file: " << ofile << std::endl;    
+    LL.sort();
 
-    std::vector<int> oname  = EncodeStringForFortran(ofile);
-    int              olen   = oname.size();
+    LL.unique();
 
-    FORT_WRITE(block,
-               &imax,
-               &jmax,
-               &kmax,
-               &ncomp,
-               fluct,
-               &scalex,
-               &scaley,
-               &scalez,
-               &oname[0],
-               &olen);
+    if (LL.empty())
+    {
+        std::cout << "No data falls within the specified range!\n";
+        usage();
+    }
+
+    std::cout << '\n' << ofile
+              << " will have "
+              << LL.size()
+              << " platters of data!\n\n";
+
+    std::cout << "Time       File\n";
+    std::cout << "---------------\n";
+
+    for (std::list<Line>::const_iterator it = LL.begin(); it != LL.end(); ++it)
+    {
+        std::cout << it->m_time << '\t' << it->m_name << '\n';
+    }
+    std::cout << '\n';
+
+    if (!BoxLib::UtilCreateDirectory(ofile, 0755))
+        BoxLib::CreateDirectoryFailed(ofile);
+
+    std::string Hdr = ofile; Hdr += "/HDR";
+    std::string Dat = ofile; Dat += "/DAT";
+
+    std::ofstream ohdr, odat;
+
+    ohdr.open(Hdr.c_str(), std::ios::out|std::ios::trunc);
+    if (!ohdr.good())
+        BoxLib::FileOpenFailed(Hdr);
+
+    odat.open(Dat.c_str(), std::ios::out|std::ios::trunc);
+    if (!odat.good())
+        BoxLib::FileOpenFailed(Dat);
+    //
+    // Gotta open a FAB to get NX and NY.
+    //
+    FArrayBox fab;
+
+    GetFab(LL.front().m_name, fab);
+    
+    const int NX = fab.box().length(0);
+    const int NY = fab.box().length(1);
+    const int NZ = LL.size();
+
+    ohdr << NX << ' '
+         << NY << ' '
+         << NZ << '\n';
+
+    ohdr << DX[0] << ' '
+         << DX[1] << ' '
+         << DX[2] << '\n';
+
+    ohdr << 1 << ' ' << 1 << ' ' << 1 << '\n';
+
+    for (std::list<Line>::const_iterator it = LL.begin(); it != LL.end(); ++it)
+    {
+        ohdr << it->m_time << '\n';
+    }
+    //
+    // Write out the first FABs info.
+    //
+    for (int i = 0; i < 3; i++)
+    {
+        ohdr << odat.tellp() << '\n';
+        //
+        // Write the FAB to the data file.
+        //
+        fab.writeOn(odat,i,1);
+    }
+    //
+    // Now get the rest of the FABs;
+    //
+    std::list<Line>::const_iterator it = LL.begin();
+
+    ++it; // Skip past first FAB.
+
+    for ( ; it != LL.end(); ++it)
+    {
+        GetFab(it->m_name, fab);
+
+        if (fab.box().length(0) != NX)
+        {
+            std::cout << "FAB does not have correct X dimension!\n";
+            usage();
+        }
+        if (fab.box().length(1) != NY)
+        {
+            std::cout << "FAB does not have correct Y dimension!\n";
+            usage();
+        }
+        if (fab.box().length(2) != 1)
+        {
+            std::cout << "FAB does not have correct Z dimension!\n";
+            usage();
+        }
+
+        for (int i = 0; i < 3; i++)
+        {
+            ohdr << odat.tellp() << '\n';
+            //
+            // Write the FAB to the data file.
+            //
+            fab.writeOn(odat,i,1);
+        }
+    }
 
     BoxLib::Finalize();
 
