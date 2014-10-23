47798c47c62e2b7341a59d54d378e047c229576a
Matthew Emmett <mwemmett@lbl.gov>
2014-10-23 13:04:16 -0700

    BPM: Aggregate forces before spreading them.

--- Source/BPM.cpp
+++ Source/BPM.cpp
@@ -1,4 +1,5 @@
 
+#include <algorithm>
 #include <cmath>
 #include <iostream>
 
@@ -11,28 +12,39 @@ typedef ParticleContainer<2*BL_SPACEDIM>::PMap PMap;
 typedef ParticleContainer<2*BL_SPACEDIM>::ParticleType ParticleType;
 
 extern "C" {
-  void bpm_compute_forces_2d(double const *x1, double const *x2,
-                             double sconstant, double slength,
-                             double blob_diam, int blob_patch_size,
-                             int const *lo, int const *hi, double const *xlo, double const *xhi, double dx,
-                             double *f, double *divf);
+  void bpm_compute_forces_2d(double const *x1, double const *v1, double *f1,
+                             double const *x2, double const *v2, double *f2,
+                             double sconstant, double slength);
+
+  void bpm_spread_forces_2d(double const *x1, double const *f1, double blob_diameter, int blob_patch_size,
+                            int const *lo, int const *hi, double const *xlo, double const *xhi, double dx,
+                            double *f, double *divf);
 }
 
 //
 // Compute spring forces given particle locations.
 //
-void BPM::ComputeForces(ParticleContainer<2*BL_SPACEDIM>& particles, MultiFab& force, Geometry& geom, int lev, int fcomp)
+void BPM::ComputeForces(ParticleContainer<2*BL_SPACEDIM>& particles, MultiFab& f, MultiFab& divf, Geometry& geom, int lev)
 {
   BL_PROFILE("BPM::ComputeForces()");
 
   const PMap& pmap = particles.GetParticles(lev);
 
   //
-  // Get all particle IDs and locations.  This is a global (parallel)
-  // operation.
+  // Get all particle IDs, locations, and velocities.  These are
+  // global (parallel) operations.
   //
-  Array<int> ids; particles.GetParticleIDs(ids);
-  Array<Real> locs; particles.GetParticleLocations(locs);
+  Array<int> ids;
+  Array<Real> locs;
+  Array<Real> vels;
+  Array<Real> frcs;
+
+  particles.GetParticleIDs(ids);
+  particles.GetParticleLocations(locs);
+  particles.GetParticleVelocities(vels);
+  frcs.resize(locs.size());
+  fill(frcs.begin(), frcs.end(), 0.0);
+  // frcs.fill(0.0);
 
   //
   // Build mapping from particle ID to index into the particle
@@ -46,18 +58,12 @@ void BPM::ComputeForces(ParticleContainer<2*BL_SPACEDIM>& particles, MultiFab& f
   for (int i=0; i<ids.size(); i++)
     idmap.insert(pair<int,int>(ids[i],i));
 
-  MultiFab divf(force.boxArray(), 1, 2);
-
   //
-  // Loop through particles and compute forces...
+  // Loop through particles and accumulate forces.
   //
   for (typename PMap::const_iterator pmap_it = pmap.begin(), pmapEnd = pmap.end(); pmap_it != pmapEnd; ++pmap_it) {
-    const int   grid = pmap_it->first;
     const PBox& pbox = pmap_it->second;
     const int   n    = pbox.size();
-
-    FArrayBox& ffab = force[grid];
-    FArrayBox& divffab = divf[grid];
 
     for (int i = 0; i < n; i++) {
       const ParticleType& p = pbox[i];
@@ -74,18 +80,42 @@ void BPM::ComputeForces(ParticleContainer<2*BL_SPACEDIM>& particles, MultiFab& f
         int i1 = (spring.p1 == id) ? idmap[spring.p1] : idmap[spring.p2];
         int i2 = (spring.p1 == id) ? idmap[spring.p2] : idmap[spring.p1];
 
-        double blob_diameter = 123.0;
-        int blob_patch_size = 2;
-
-        // as it is now, this doesn't accumulate forces and then apply
-        // the bpm, it applies the bpm to each end of each spring...
-
-        bpm_compute_forces_2d(&locs[BL_SPACEDIM*i1], &locs[BL_SPACEDIM*i2],
-                              spring.k, spring.l, blob_diameter, blob_patch_size,
-                              ffab.loVect(), ffab.hiVect(), geom.ProbLo(), geom.ProbHi(), geom.CellSize()[0],
-                              ffab.dataPtr(), divffab.dataPtr());
+        // need some way to not double count...
+        bpm_compute_forces_2d(&locs[BL_SPACEDIM*i1], &vels[BL_SPACEDIM*i1], &frcs[BL_SPACEDIM*i1],
+                              &locs[BL_SPACEDIM*i2], &vels[BL_SPACEDIM*i2], &frcs[BL_SPACEDIM*i2],
+                              spring.k, spring.l);
 
       }
+    }
+  }
+
+  //
+  // Loop through particles and spread forces.
+  //
+  for (typename PMap::const_iterator pmap_it = pmap.begin(), pmapEnd = pmap.end(); pmap_it != pmapEnd; ++pmap_it) {
+    const int   grid = pmap_it->first;
+    const PBox& pbox = pmap_it->second;
+    const int   n    = pbox.size();
+
+    FArrayBox& ffab = f[grid];
+    FArrayBox& divffab = divf[grid];
+
+    for (int i = 0; i < n; i++) {
+      const ParticleType& p = pbox[i];
+
+      const int id = p.m_id;
+      if (id <= 0) continue;
+
+      int i1 = idmap[id];
+
+      double blob_diameter   = 0.05;
+      int    blob_patch_size = ceil(blob_diameter/2 / geom.CellSize()[0]) + 1;
+
+      bpm_spread_forces_2d(&locs[BL_SPACEDIM*i1], &frcs[BL_SPACEDIM*i1],
+                           blob_diameter, blob_patch_size,
+                           ffab.loVect(), ffab.hiVect(), geom.ProbLo(), geom.ProbHi(), geom.CellSize()[0],
+                           ffab.dataPtr(), divffab.dataPtr());
+
     }
   }
 
--- Source/bpm.f90
+++ Source/bpm.f90
@@ -3,17 +3,12 @@ module bpm
   implicit none
 contains
 
-  subroutine bpm_compute_forces_2d(x1, x2, sconstant, slength, diam, L, lo, hi, xlo, xhi, dx, f, divf) bind(c)
-    real(c_double), intent(in   )        :: x1(2), x2(2), xlo(2), xhi(2)
-    real(c_double), intent(in   ), value :: sconstant, slength, diam, dx
-    integer(c_int), intent(in   ), value :: L
-    integer(c_int), intent(in   )        :: lo(2), hi(2)
-    real(c_double), intent(inout)        :: f(lo(1):hi(1),lo(2):hi(2),2), divf(lo(1):hi(1),lo(2):hi(2))
+  subroutine bpm_compute_forces_2d(x1, v1, f1, x2, v2, f2, sconstant, slength) bind(c)
+    real(c_double), intent(in   )        :: x1(2), v1(2), x2(2), v2(2)
+    real(c_double), intent(inout)        :: f1(2), f2(2)
+    real(c_double), intent(in   ), value :: sconstant, slength
 
-    real(c_double), dimension(-L:L,-L:L) :: distx, disty, dist2, blob, dbdr
-
-    real(c_double) :: a, b, x, y, force(2), delta(2), distance
-    integer :: i, j, c, ij(2)
+    real(c_double) :: force(2), delta(2), distance
 
     ! compute spring force
     delta    = x2 - x1
@@ -24,16 +19,35 @@ contains
        force = 0
     end if
 
-    print *, "BPM", x1, x2, force
+    print *, "bpm:: x1:", x1
+    print *, "bpm:: x2:", x2
+    print *, "bpm:: f: ", force
+
+    f1 = f1 + force
+    f2 = f2 - force
+
+  end subroutine bpm_compute_forces_2d
+
+  subroutine bpm_spread_forces_2d(x1, force, diam, L, lo, hi, xlo, xhi, dx, f, divf) bind(c)
+    real(c_double), intent(in   )        :: x1(2), force(2), xlo(2), xhi(2)
+    real(c_double), intent(in   ), value :: diam, dx
+    integer(c_int), intent(in   ), value :: L
+    integer(c_int), intent(in   )        :: lo(2), hi(2)
+    real(c_double), intent(inout)        :: f(lo(1):hi(1),lo(2):hi(2),2), divf(lo(1):hi(1),lo(2):hi(2))
+
+    real(c_double), dimension(-L:L,-L:L) :: distx, disty, dist2, blob, dbdr
+
+    real(c_double) :: a, b, x, y
+    integer :: i, j, c, ij(2)
 
     ! compute cell that contains particle
-    ij = nint((x1 - xlo) / dx + lo - 0.5d0)
+    ij = nint((x1 - xlo) / dx + lo)! - 0.5d0)
 
     ! compute distances to cell centers
     do i = -L, L
-       x = xlo(1) + dx * (ij(1) + i - lo(1) + 0.d50)
+       x = xlo(1) + dx * (ij(1) + i - lo(1) + 0.5d0)
        do j = -L, L
-          y = xlo(2) + dx * (ij(2) + j - lo(2) + 0.d50)
+          y = xlo(2) + dx * (ij(2) + j - lo(2) + 0.5d0)
           distx(i,j) = x - x1(1)
           disty(i,j) = y - x1(2)
        end do
@@ -52,26 +66,34 @@ contains
        dbdr = 0
     end where
 
-    ! spread force
-    do c = 1, 2
-       do i = -L, L
-          do j = -L, L
-             f(ij(1)+i,ij(2)+j,c) = f(ij(1)+i,ij(2)+j,c) + force(c) * blob(i,j)
-          end do
-       end do
+    print *, "bpm:: blob volume", sum(blob)*dx**2
+    do i = -L, L
+       print *, blob(i,:)
     end do
+
+    print *, "bpm:: dbdr"
+    do i = -L, L
+       print *, dbdr(i,:)
+    end do
+
+
+    ! spread force
+    ! do c = 1, 2
+    !    do i = -L, L
+    !       do j = -L, L
+    !          f(ij(1)+i,ij(2)+j,c) = f(ij(1)+i,ij(2)+j,c) + force(c) * blob(i,j)
+    !       end do
+    !    end do
+    ! end do
 
     ! spread divergence
-    do i = -L, L
-       do j = -L, L
-          divf(ij(1)+i,ij(2)+j) = divf(ij(1)+i,ij(2)+j) + &
-               (force(1) * distx(i,j) + force(2) * disty(i,j)) * dbdr(i,j)
-       end do
-    end do
+    ! do i = -L, L
+    !    do j = -L, L
+    !       divf(ij(1)+i,ij(2)+j) = divf(ij(1)+i,ij(2)+j) + &
+    !            (force(1) * distx(i,j) + force(2) * disty(i,j)) * dbdr(i,j)
+    !    end do
+    ! end do
 
-  end subroutine bpm_compute_forces_2d
-
-  ! subroutine bpm_compute_forces_3d() bind(c)
-  ! end subroutine bpm_compute_forces_3d
+  end subroutine bpm_spread_forces_2d
 
 end module bpm
