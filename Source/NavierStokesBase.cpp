f0d8d60a1a76048e75880348b97ec76e525478d8
Marc Day <MSDay@lbl.gov>
2018-09-16 23:41:55 -0700

    Tiling bug

--- Source/NavierStokesBase.cpp
+++ Source/NavierStokesBase.cpp
@@ -1257,12 +1257,10 @@ NavierStokesBase::estTimeStep ()
 #endif
 {
     Real gr_max[BL_SPACEDIM];
+    FArrayBox tforces;
 
     for (MFIter Rho_mfi(rho_ctime,true); Rho_mfi.isValid(); ++Rho_mfi)
     {
-    
-        FArrayBox tforces;
-      
 	const Box& bx=Rho_mfi.tilebox();
         //
         // Get the velocity forcing.  For some reason no viscous forcing.
