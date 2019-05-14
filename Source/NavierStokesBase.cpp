bd853908115f34cc5dab6d9222383a74102bb40c
Marc Day <MSDay@lbl.gov>
2019-05-13 17:54:42 -0700

    Fix create_umac_grown for periodic cases

--- Source/NavierStokesBase.cpp
+++ Source/NavierStokesBase.cpp
@@ -1137,9 +1137,16 @@ NavierStokesBase::create_umac_grown (int nGrow)
     const Real* xlo = geom.ProbLo(); //these aren't actually used by the FORT method
     const Real* dx  = geom.CellSize();
 
+    Box domain_box = geom.Domain();
+    for (int idim = 0; idim < BL_SPACEDIM; ++idim) {
+        if (geom.isPeriodic(idim)) {
+            domain_box.grow(idim,nGrow);
+        }
+    }
+
     for (int n = 0; n < BL_SPACEDIM; ++n)
     {
-        Box dm = geom.Domain();
+        Box dm = domain_box;
         dm.surroundingNodes(n);
         const int*  lo  = dm.loVect();
         const int*  hi  = dm.hiVect();
@@ -1158,6 +1165,7 @@ NavierStokesBase::create_umac_grown (int nGrow)
         }
         // call FillBoundary to make sure that fine/fine grow cells are valid
 	u_mac[n].FillBoundary(geom.periodicity());
+        u_mac[n].EnforcePeriodicity(geom.periodicity());
     }
 }
 
