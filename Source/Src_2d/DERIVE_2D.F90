fdcd5f1bb298f7e6308fcd7f1edaca675e566493
Lucas Esclapez <lesclapez@lbl.gov>
2019-10-14 17:37:14 -0700

    [Feature_nd] Remove unsused dermgvort2.

--- Source/Src_2d/DERIVE_2D.F90
+++ Source/Src_2d/DERIVE_2D.F90
@@ -27,9 +27,7 @@ module derive_2d_module
   private
 
   public dermodgradrho, derkeng,derlogs, dermvel, &
-       derlgrhodust, &
-       dermgvort2, &
-       dergrdp, dernull
+       derlgrhodust, dergrdp, dernull
 
 contains
 
@@ -214,112 +212,6 @@ contains
       end do
 
     end subroutine derlgrhodust
-
-      subroutine dermgvort2 (vort,DIMS(vort),nv,dat,DIMS(dat),ncomp,&
-                                 lo,hi,domlo,domhi,delta,xlo,time,dt,&
-                                 bc,level,grid_no) bind(C,name="dermgvort2")
-      implicit none
-!c
-!c ::: This routine will derive magnitude of vorticity from
-!c ::: the velocity field
-!c
-      integer    lo(2), hi(2)
-      integer    DIMDEC(vort)
-      integer    DIMDEC(dat)
-      integer    domlo(2), domhi(2)
-      integer    nv, ncomp
-      integer    bc(2,2,ncomp)
-      REAL_T     delta(2), xlo(2), time, dt
-      REAL_T     vort(DIMV(vort),nv)
-      REAL_T     dat(DIMV(dat),ncomp)
-      integer    level, grid_no
-
-      integer   i,j
-      REAL_T    uy, vx, dx, dy
-      REAL_T    uycen, uylo, uyhi
-      REAL_T    vxcen, vxlo, vxhi
-      REAL_T    vorfun
-!c
-!c     ::::: some useful macro definitions
-!c
-#     define U(i,j) dat(i,j,1)
-#     define V(i,j) dat(i,j,2)
-
-#     define ULOY bc(2,1,1)
-#     define UHIY bc(2,2,1)
-
-#     define VLOX bc(1,1,2)
-#     define VHIX bc(1,2,2)
-!c
-!c     ::::: statement functions that implement stencil
-!c
-      uycen(i,j) = half*(U(i,j+1)-U(i,j-1))/dy
-      uylo(i,j) = (eight*U(i,j)-six*U(i,j+1)+U(i,j+2))/(three*dy)
-      uyhi(i,j) = (eight*U(i,j)-six*U(i,j-1)+U(i,j-2))/(three*dy)
-
-      vxcen(i,j) = half*(V(i+1,j)-V(i-1,j))/dx
-      vxlo(i,j) = (eight*V(i,j)-six*V(i+1,j)+V(i+2,j))/(three*dx)
-      vxhi(i,j) = (eight*V(i,j)-six*V(i-1,j)+V(i-2,j))/(three*dx)
-
-      vorfun(vx,uy) = vx - uy
-
-      dx = delta(1)
-      dy = delta(2)
-!c
-!c     :: at physical bndries where an edge value is prescribed,
-!c     :: set the value in the outside cell so that a central
-!c     :: difference formula is equivalent to the higher order
-!c     :: one sided formula
-!c
-      if (lo(1) .eq. domlo(1)) then
-         i = lo(1)
-         if (VLOX.eq.EXT_DIR .or. VLOX.eq.HOEXTRAP) then
-	   do j = lo(2), hi(2)
-	      V(i-1,j) = vxlo(i,j)
-	   end do
-	 end if
-      end if
-      if (hi(1) .eq. domhi(1)) then
-         i = hi(1)
-         if (VHIX.eq.EXT_DIR .or. VHIX.eq.HOEXTRAP) then
-	   do j = lo(2), hi(2)
-	      V(i+1,j) = vxhi(i,j)
-	   end do
-	 end if
-      end if
-      if (lo(2) .eq. domlo(2)) then
-         j = lo(2)
-	 if (ULOY.eq.EXT_DIR .or. ULOY.eq.HOEXTRAP) then
-	   do i = lo(1), hi(1)
-	      U(i,j-1) = uylo(i,j)
-	   end do
-	 end if
-      end if
-      if (hi(2) .eq. domhi(2)) then
-         j = hi(2)
-	 if (UHIY.eq.EXT_DIR .or. UHIY.eq.HOEXTRAP) then
-	   do i = lo(1), hi(1)
-	      U(i,j+1) = uyhi(i,j)
-	   end do
-	 end if
-      end if
-
-      do j = lo(2), hi(2)
-         do i = lo(1), hi(1)
-            uy = uycen(i,j)
-            vx = vxcen(i,j)
-            vort(i,j,1) = vorfun(vx,uy)
-         end do
-      end do
-
-#     undef U
-#     undef V      
-#     undef ULOY
-#     undef UHIY
-#     undef VLOX
-#     undef VHIX
-
-    end subroutine dermgvort2
 
 !c=========================================================
 
