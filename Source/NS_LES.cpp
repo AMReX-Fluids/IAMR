6cd6b21794d1680108a07e6ee039f188dd63a142
Emmanuel Motheau <emotheau@lbl.gov>
2020-03-05 16:24:28 -0800

    ok the framework is ready to start to implement a new routine in the TensorOP object to compute gradients

--- Source/NS_LES.cpp
+++ Source/NS_LES.cpp
@@ -4,6 +4,8 @@
 
 
 #include <NavierStokesBase.H>
+#include <AMReX_VisMF.H>
+
 
 
 #include <AMReX_MLMG.H>
@@ -36,11 +38,11 @@ NavierStokesBase::calc_mut_LES(const Real time)
   auto whichTime = which_time(State_Type,time);
   BL_ASSERT(whichTime == AmrOldTime || whichTime == AmrNewTime);
 
-  MultiFab& U = (whichTime == AmrOldTime) ? get_old_data(State_Type) : get_new_data(State_Type);
+  MultiFab& Sstate = (whichTime == AmrOldTime) ? get_old_data(State_Type) : get_new_data(State_Type);
   
   int nGrow = 1;
-  FillPatchIterator fpi(*this,U,nGrow,time,State_Type,Xvel,AMREX_SPACEDIM);
-  
+  FillPatchIterator fpi(*this,Sstate,nGrow,time,State_Type,Xvel,AMREX_SPACEDIM);
+  MultiFab& Uvel=fpi.get_mf();
   
 
 
@@ -73,96 +75,62 @@ NavierStokesBase::calc_mut_LES(const Real time)
       Array<LinOpBCType,AMREX_SPACEDIM> mlmg_lobc[AMREX_SPACEDIM];
       Array<LinOpBCType,AMREX_SPACEDIM> mlmg_hibc[AMREX_SPACEDIM];
       // fill it
-      for (int i=0; i<AMREX_SPACEDIM; i++)
-        Diffusion::setDomainBC(mlmg_lobc[i], mlmg_hibc[i], Xvel+i);
+      for (int i=0; i<AMREX_SPACEDIM; i++){
+        LES_setDomainBC(mlmg_lobc[i], mlmg_hibc[i], Xvel+i);
+      }    
+        
       // pass to op
       tensorop.setDomainBC({AMREX_D_DECL(mlmg_lobc[0],mlmg_lobc[1],mlmg_lobc[2])},
                            {AMREX_D_DECL(mlmg_hibc[0],mlmg_hibc[1],mlmg_hibc[2])});  
   
-#if 0
-
-  
+     
+      
       // set up level BCs
-      {
-        MultiFab crsedata;
-        int ng = 0;
-
-        if (level > 0) {
-          auto& crse_ns = *(coarser->navier_stokes);
-          crsedata.define(crse_ns.boxArray(), crse_ns.DistributionMap(), AMREX_SPACEDIM,
-                          ng, MFInfo(),crse_ns.Factory());
-          AmrLevel::FillPatch(crse_ns, crsedata, ng, cur_time, State_Type, Xvel,
-                              AMREX_SPACEDIM);
-          tensorop.setCoarseFineBC(&crsedata, crse_ratio[0]);
-        }
-
-        AmrLevel::FillPatch(*navier_stokes,Soln,soln_ng,cur_time,State_Type,Xvel,AMREX_SPACEDIM);
-
-        tensorop.setLevelBC(0, &Soln);
-      }
-
-      {
-        MultiFab acoef;
-        acoef.define(grids, dmap, 1, 0, MFInfo(), navier_stokes->Factory());
-        std::pair<Real,Real> scalars;
-        // fixme? don't know why we're bothering with rhsscale....
-        Real rhsscale = 1.0;
-        const MultiFab& rho = (rho_flag == 1) ? rho_half : navier_stokes->rho_ctime;
-
-        computeAlpha(acoef, scalars, a, b, rho,
-                     1, &rhsscale, nullptr, 0,
-                     nullptr, 0,
-                     navier_stokes->Geom(),volume,parent->Geom(0).IsRZ());
-
-        tensorop.setScalars(scalars.first, scalars.second);
-        tensorop.setACoeffs(0, acoef);
-      }
-
-
-
-{
-        Array<MultiFab,AMREX_SPACEDIM> face_bcoef;
-        for (int n = 0; n < BL_SPACEDIM; n++)
-        {
-          face_bcoef[n].define(area[n].boxArray(),area[n].DistributionMap(),1,0,MFInfo(),navier_stokes->Factory());
-        }
-
-        //      computeBeta(face_bcoef,betan,betaComp);
-        computeBeta(face_bcoef,betan,betaComp,navier_stokes->Geom(),ap,
-                    parent->Geom(0).IsRZ());
-        tensorop.setShearViscosity(0, amrex::GetArrOfConstPtrs(face_bcoef));
-
+      // WARNING THIS PART BELOW HAS NOT BEEN TESTED
+      //{
+      //  MultiFab crsedata;
+      //  int ng = 0;
+      //  const int soln_ng = 1;
+      //
+      //  if (level > 0) {
+      //    //auto& crse_ns = *(coarser->navier_stokes);
+      //    NavierStokesBase& crse_ns  = getLevel(level-1);
+      //    crsedata.define(crse_ns.boxArray(), crse_ns.DistributionMap(), AMREX_SPACEDIM,
+      //                    ng, MFInfo(),crse_ns.Factory());
+      //    AmrLevel::FillPatch(crse_ns, crsedata, ng, time, State_Type, Xvel,
+      //                        AMREX_SPACEDIM);
+      //    tensorop.setCoarseFineBC(&crsedata, crse_ratio[0]);
+      //  }
+      //
+      //  AmrLevel::FillPatch(*this,Uvel,soln_ng,time,State_Type,Xvel,AMREX_SPACEDIM);
+      //
+        tensorop.setLevelBC(0, &Uvel);
+      //}
+      
+      
+        tensorop.setScalars(0., 1.0);
+        tensorop.setACoeffs(0, 0.0);
+        tensorop.setShearViscosity(0, [1. ,1.]);
+        tensorop.setBulkViscosity(0, 0.);
 #ifdef AMREX_USE_EB
-        MultiFab cc_bcoef(grids,dmap,BL_SPACEDIM,0,MFInfo(),navier_stokes->Factory());
-        EB_average_face_to_cellcenter(cc_bcoef, 0,
-                                      amrex::GetArrOfConstPtrs(face_bcoef));
-        tensorop.setEBShearViscosity(0, cc_bcoef);
+        tensorop.setEBShearViscosity(0, 1.);
+        tensorop.setEBBulkViscosity(0, 0.);
 #endif
 
-        if (NavierStokesBase::S_in_vel_diffusion) {
-          // remove the "divmusi" terms by setting kappa = (2/3) mu
-          //
-          Print()<<"WARNING: Hack to get rid of divU terms ...\n";
-          Array<MultiFab,AMREX_SPACEDIM> kappa;
-          Real twothirds = 2.0/3.0;
-          for (int idim = 0; idim < AMREX_SPACEDIM; ++idim)
-          {
-            kappa[idim].define(face_bcoef[idim].boxArray(), face_bcoef[idim].DistributionMap(), 1, 0, MFInfo(),navier_stokes->Factory());
-            MultiFab::Copy(kappa[idim], face_bcoef[idim], 0, 0, 1, 0);
-            kappa[idim].mult(twothirds);
-          }
-          tensorop.setBulkViscosity(0, amrex::GetArrOfConstPtrs(kappa));
-#ifdef AMREX_USE_EB
-          cc_bcoef.mult(twothirds);
-          tensorop.setEBBulkViscosity(0, cc_bcoef);
-#endif
-        }
-      }  
 
 
 
- #endif 
-  
+ FluxBoxes fb(this,AMREX_SPACEDIM);
+ MultiFab** tensorflux = fb.get();
+std::array<MultiFab*,AMREX_SPACEDIM> fp{AMREX_D_DECL(tensorflux[0], tensorflux[1], tensorflux[2])};
+
+  // OK READY TO CREATE THE ROUTINE TO COMPUTE GRADIENT INSTEAD OF CALLING COMPFLUX 
+ tensorop.compFlux(0,{fp},{Uvel},MLLinOp::Location::FaceCenter);
+ 
+ //VisMF::Write(Uvel,"test");
+ 
+  VisMF::Write(*fp[0],"test");
+
   
   if (LES_model == "Smagorinsky") {
     amrex::Print() << "\n DEBUG WE DO SMAGORNISKY \n\n";
@@ -172,4 +140,64 @@ NavierStokesBase::calc_mut_LES(const Real time)
     amrex::Abort("\n DEBUG DONT KNOW THIS LES MODEL \n\n");
   }
   
+}
+
+
+
+
+void
+NavierStokesBase::LES_setDomainBC (std::array<LinOpBCType,AMREX_SPACEDIM>& mlmg_lobc,
+                        std::array<LinOpBCType,AMREX_SPACEDIM>& mlmg_hibc,
+                        int src_comp)
+{
+    const BCRec& bc = get_desc_lst()[State_Type].getBC(src_comp);
+    for (int idim = 0; idim < AMREX_SPACEDIM; ++idim)
+    {
+        if (parent->Geom(0).isPeriodic(idim))
+        {
+            mlmg_lobc[idim] = mlmg_hibc[idim] = LinOpBCType::Periodic;
+        }
+        else
+        {
+            int pbc = bc.lo(idim);
+            if (pbc == EXT_DIR)
+            {
+                mlmg_lobc[idim] = LinOpBCType::Dirichlet;
+            }
+            else if (pbc == FOEXTRAP      ||
+                     pbc == HOEXTRAP      ||
+                     pbc == REFLECT_EVEN)
+            {
+                mlmg_lobc[idim] = LinOpBCType::Neumann;
+            }
+            else if (pbc == REFLECT_ODD)
+            {
+                mlmg_lobc[idim] = LinOpBCType::reflect_odd;
+            }
+            else
+            {
+                mlmg_lobc[idim] = LinOpBCType::bogus;
+            }
+
+            pbc = bc.hi(idim);
+            if (pbc == EXT_DIR)
+            {
+                mlmg_hibc[idim] = LinOpBCType::Dirichlet;
+            }
+            else if (pbc == FOEXTRAP      ||
+                     pbc == HOEXTRAP      ||
+                     pbc == REFLECT_EVEN)
+            {
+                mlmg_hibc[idim] = LinOpBCType::Neumann;
+            }
+            else if (pbc == REFLECT_ODD)
+            {
+                mlmg_hibc[idim] = LinOpBCType::reflect_odd;
+            }
+            else
+            {
+                mlmg_hibc[idim] = LinOpBCType::bogus;
+            }
+        }
+    }
 }
