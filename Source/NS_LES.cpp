46264a1beb38c694d3d8497afe965992665a26f3
Emmanuel Motheau <emotheau@lbl.gov>
2020-03-11 16:01:10 -0700

    LES seems to run in 2D and  3D more validation needed

--- Source/NS_LES.cpp
+++ Source/NS_LES.cpp
@@ -1,12 +1,11 @@
 
 
-
-
+//fixme, for writesingle level plotfile
+#include<AMReX_PlotFileUtil.H>
+//
 
 #include <NavierStokesBase.H>
 #include <AMReX_VisMF.H>
-
-
 
 #include <AMReX_MLMG.H>
 #ifdef AMREX_USE_EB
@@ -32,8 +31,12 @@ NavierStokesBase::calc_mut_LES(MultiFab* mu_LES[BL_SPACEDIM], const Real time)
 {
 
   if (ParallelDescriptor::IOProcessor() && getLESVerbose) {
-    amrex::Print() << "\n DEBUG WE ARE IN THE NEW ROUTINE FOR LES \n\n";
+    amrex::Print() << "\n in calc_mut_LES : WE ARE IN THE NEW ROUTINE FOR LES \n\n";
   }
+  
+  //
+  // We get the state data at the current time in order to get the velocity
+  //
   
   auto whichTime = which_time(State_Type,time);
   BL_ASSERT(whichTime == AmrOldTime || whichTime == AmrNewTime);
@@ -44,7 +47,10 @@ NavierStokesBase::calc_mut_LES(MultiFab* mu_LES[BL_SPACEDIM], const Real time)
   FillPatchIterator fpi(*this,Sstate,nGrow,time,State_Type,Xvel,AMREX_SPACEDIM);
   MultiFab& Uvel=fpi.get_mf();
   
-
+  //
+  // Creating the TensorOp object to compute gradients of velocity at each face
+  //
+  
   LPInfo info;
 
 #ifdef AMREX_USE_EB
@@ -56,144 +62,119 @@ NavierStokesBase::calc_mut_LES(MultiFab* mu_LES[BL_SPACEDIM], const Real time)
   MLTensorOp tensorop({geom}, {grids}, {dmap}, info);
 #endif
 
-      // create right container
-      Array<LinOpBCType,AMREX_SPACEDIM> mlmg_lobc[AMREX_SPACEDIM];
-      Array<LinOpBCType,AMREX_SPACEDIM> mlmg_hibc[AMREX_SPACEDIM];
-      // fill it
-      for (int i=0; i<AMREX_SPACEDIM; i++){
-        LES_setDomainBC(mlmg_lobc[i], mlmg_hibc[i], Xvel+i);
-      }    
+  // create right container
+  Array<LinOpBCType,AMREX_SPACEDIM> mlmg_lobc[AMREX_SPACEDIM];
+  Array<LinOpBCType,AMREX_SPACEDIM> mlmg_hibc[AMREX_SPACEDIM];
+  // fill it
+  for (int i=0; i<AMREX_SPACEDIM; i++){
+    LES_setDomainBC(mlmg_lobc[i], mlmg_hibc[i], Xvel+i);
+  }    
         
-      // pass to op
-      tensorop.setDomainBC({AMREX_D_DECL(mlmg_lobc[0],mlmg_lobc[1],mlmg_lobc[2])},
-                           {AMREX_D_DECL(mlmg_hibc[0],mlmg_hibc[1],mlmg_hibc[2])});  
+  // pass to op
+  tensorop.setDomainBC({AMREX_D_DECL(mlmg_lobc[0],mlmg_lobc[1],mlmg_lobc[2])},
+                       {AMREX_D_DECL(mlmg_hibc[0],mlmg_hibc[1],mlmg_hibc[2])});  
   
-     
+  // set up level BCs
+  {
+    MultiFab crsedata;
+    int ng = 0;
+    const int soln_ng = 1;
       
-      // set up level BCs
-      // WARNING THIS PART BELOW HAS NOT BEEN TESTED
-      {
-        MultiFab crsedata;
-        int ng = 0;
-        const int soln_ng = 1;
+    if (level > 0) {
+      //auto& crse_ns = *(coarser->navier_stokes);
+      NavierStokesBase& crse_ns  = getLevel(level-1);
+      crsedata.define(crse_ns.boxArray(), crse_ns.DistributionMap(), AMREX_SPACEDIM,
+                      ng, MFInfo(),crse_ns.Factory());
+      AmrLevel::FillPatch(crse_ns, crsedata, ng, time, State_Type, Xvel,
+                          AMREX_SPACEDIM);
+      tensorop.setCoarseFineBC(&crsedata, crse_ratio[0]);
+     }
       
-        if (level > 0) {
-          //auto& crse_ns = *(coarser->navier_stokes);
-          NavierStokesBase& crse_ns  = getLevel(level-1);
-          crsedata.define(crse_ns.boxArray(), crse_ns.DistributionMap(), AMREX_SPACEDIM,
-                          ng, MFInfo(),crse_ns.Factory());
-          AmrLevel::FillPatch(crse_ns, crsedata, ng, time, State_Type, Xvel,
-                              AMREX_SPACEDIM);
-          tensorop.setCoarseFineBC(&crsedata, crse_ratio[0]);
-        }
+     AmrLevel::FillPatch(*this,Uvel,soln_ng,time,State_Type,Xvel,AMREX_SPACEDIM);
       
-        AmrLevel::FillPatch(*this,Uvel,soln_ng,time,State_Type,Xvel,AMREX_SPACEDIM);
-      
-        tensorop.setLevelBC(0, &Uvel);
-      }
-      
-      
- const int dim_fluxes = pow(AMREX_SPACEDIM,2);
- FluxBoxes fb(this,dim_fluxes);
- MultiFab** tensorflux = fb.get();
-std::array<MultiFab*,AMREX_SPACEDIM> grad_Uvel{AMREX_D_DECL(tensorflux[0], tensorflux[1], tensorflux[2])};
-
-  // OK READY TO CREATE THE ROUTINE TO COMPUTE GRADIENT INSTEAD OF CALLING COMPFLUX 
-// tensorop.compFlux(0,{fp},{Uvel},MLLinOp::Location::FaceCenter);
+     tensorop.setLevelBC(0, &Uvel);
+  }
+              
+  const int dim_fluxes = pow(AMREX_SPACEDIM,2);
+  
+  FluxBoxes fb(this,dim_fluxes);
+  MultiFab** tensorflux = fb.get();
+  std::array<MultiFab*,AMREX_SPACEDIM> grad_Uvel{AMREX_D_DECL(tensorflux[0], tensorflux[1], tensorflux[2])};
  
-tensorop.compVelGrad(0,{grad_Uvel},{Uvel},MLLinOp::Location::FaceCenter);
+  tensorop.compVelGrad(0,{grad_Uvel},{Uvel},MLLinOp::Location::FaceCenter);
 
-
-// VisMF::Write(Uvel,"test");
- 
-  VisMF::Write(*grad_Uvel[0],"fluxes_x");
-  VisMF::Write(*grad_Uvel[1],"fluxes_y");
+//VisMF::Write(*grad_Uvel[0],"fluxes_x");
+//VisMF::Write(*grad_Uvel[1],"fluxes_y");
+  
+//static int count_LES_src=0;
+//count_LES_src++;
+//amrex::WriteSingleLevelPlotfile("grad_Uvel_"+std::to_string(count_LES_src), *grad_Uvel[0], {"comp0","comp1","comp2","comp3"}, geom, 0.0, 0);
 
   
-  if (LES_model == "Smagorinsky") {
-    amrex::Print() << "\n DEBUG WE DO SMAGORNISKY with constant " << smago_Cs_cst << "\n\n";
+ 
+ 
+  //
+  // Now that we have the gradients of velocity, we can compute the LES subgrid viscosity
+  // 
+      
+  const auto dx = geom.CellSizeArray();
 
-const auto dx = geom.CellSizeArray();
- //amrex::Print() << "\n DEBUG geom.CellSizeArray() " << dx[0]  << "\n\n";
+// amrex::Print() << "\n DEBUG geom.CellSizeArray() " << dx[0]  << "\n\n";
 
-FArrayBox fab_tmp;
-for (MFIter mfi(Uvel,true); mfi.isValid(); ++mfi)
+  FArrayBox fab_tmp;
+  for (MFIter mfi(Uvel,true); mfi.isValid(); ++mfi)
   {
     Box bx = mfi.tilebox();
 
-// need face-centered tilebox for each direction
-    //D_TERM(const Box& xbx = mfi.tilebox(IntVect::TheDimensionVector(0));,
-    //       const Box& ybx = mfi.tilebox(IntVect::TheDimensionVector(1));,
-    //       const Box& zbx = mfi.tilebox(IntVect::TheDimensionVector(2)););
-
-
-for (int idim = 0; idim < AMREX_SPACEDIM; ++idim) {
+    for (int idim = 0; idim < AMREX_SPACEDIM; ++idim) {
   
-  amrex::Print() << "\n WE ARE IN IDIM " << idim << "\n ";
+  //amrex::Print() << "\n WE ARE IN IDIM " << idim << "\n ";
   
-                const Box& nbx = mfi.nodaltilebox(idim);
-                Array4<Real      > dst = mu_LES[idim]->array(mfi);
-                Array4<Real      > src = grad_Uvel[idim]->array(mfi);
+      const Box& nbx = mfi.nodaltilebox(idim);
+      Array4<Real      > dst = mu_LES[idim]->array(mfi);
+      Array4<Real      > src = grad_Uvel[idim]->array(mfi);
 
-                AMREX_HOST_DEVICE_PARALLEL_FOR_4D (nbx, 1, i, j, k, n,
-                {
-                  
-                  Real smag = 0;
-                  for (int i_symij = 0; i_symij < dim_fluxes; ++i_symij)
-                  {
-                    Real symij = src(i,j,k,i_symij) + src(i,j,k,i_symij);
-                    smag += symij * symij;
-                  }
-                  
-                  smag = 0.5 * smag;
-                  
-                  
-                  //amrex::Print() << "\n component 0 " << src(i,j,k,0);
-                  //amrex::Print() << "\n component 1 " << src(i,j,k,1);
-                  //amrex::Print() << "\n component 2 " << src(i,j,k,2);
-                  //amrex::Print() << "\n component 3 " << src(i,j,k,3);
-                 
-                  
-                    dst(i,j,k,n) = pow(smago_Cs_cst * dx[idim],2) * sqrt(smag);
-                });
+      AMREX_HOST_DEVICE_PARALLEL_FOR_4D (nbx, 1, i, j, k, n,
+      {
+         if (LES_model == "Smagorinsky") {
+          
+            if (ParallelDescriptor::IOProcessor() && getLESVerbose) {               
+                amrex::Print() << "\n in calc_mut_LES : WE DO SMAGORNISKY with constant " << smago_Cs_cst << "\n\n";
             }
-
-
-
-//      for (int dir=0; dir<AMREX_SPACEDIM; dir++) {
-//
-//        Box ebox = surroundingNodes(bx,dir);
-//        fab_tmp.resize(ebox,1);
-//
-//// HAVE TO MAKE A 4D LOOP HERE see MLTensorOp::compVelGrad
-//
-//        for (int k = 0; k < dim_fluxes; k++)
-//        {
-//          fab_tmp.setVal(200.);
-////          fab_tmp.copy( (*flux[i])[mfi],ebox,k,ebox,0,1);
-////          fab_tmp.mult((enth_edgstate[i])[mfi],ebox,k,0,1);
-//          (*mu_LES[dir])[mfi].plus(fab_tmp,0,0,1);
-//        }
-//      }
-//      
-      
-      
-}
-
-
-VisMF::Write(*mu_LES[0],"mu_LES_x");
-VisMF::Write(*mu_LES[1],"mu_LES_y");
-
-//            for (int dir=0; dir<AMREX_SPACEDIM; dir++) {
-//                mu_LES[dir]->setVal(999., 0, mu_LES[dir]->nComp(), mu_LES[dir]->nGrow());
-//            }
-
-
+          
+            Real smag = 0;
+            for (int i_symij = 0; i_symij < dim_fluxes; ++i_symij)
+            {
+              Real symij = src(i,j,k,i_symij) + src(i,j,k,i_symij);
+              smag += symij * symij;
+            }
+                  
+            smag = 0.5 * smag;
+                  
+            dst(i,j,k,n) = pow(smago_Cs_cst * dx[idim],2) * sqrt(smag);
+                    
+         }
+         else
+         {
+           amrex::Abort("\n DEBUG DONT KNOW THIS LES MODEL \n\n");
+         }
+                    
+         //amrex::Print() << "\n MU_LES " << i << " " << j << " " << k << " " << dst(i,j,k,n);
+      });
+    }
+    
   }
-  else
-  {
-    amrex::Abort("\n DEBUG DONT KNOW THIS LES MODEL \n\n");
-  }
+
+//static int count_LES=0;
+//count_LES++;
+//amrex::WriteSingleLevelPlotfile("mu_LES_"+std::to_string(count_LES), *mu_LES[0], {"mu_LES_x"}, geom, 0.0, 0);
+//
+
+
+//VisMF::Write(*mu_LES[0],"mu_LES_x");
+//VisMF::Write(*mu_LES[1],"mu_LES_y");
+
+ 
   
 }
 
