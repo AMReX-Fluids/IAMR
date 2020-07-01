8c2364cb2aa1eec04168e7b1bcd02691e27f43ac
Michele Rosso <mrosso@lbl.gov>
2020-06-30 17:52:13 -0700

    Revert "Godunov: this is needed to match IAMR and incflo solution for dt=0.0"
    This reverts commit c41b764c8cecf3210b1e012edf1d321e3b3f37ff.

--- Source/Godunov/iamr_godunov_plm.cpp
+++ Source/Godunov/iamr_godunov_plm.cpp
@@ -59,14 +59,6 @@ void godunov::predict_plm_x (Box const& bx_in, int ncomp,
             Real umns = q(i-1,j,k,n) + 0.5 * ( 1.0 - vcc(i-1,j,k,0) * dtdx) *
                 incflo_ho_xslope_extdir(i-1,j,k,n,q, extdir_ilo, extdir_ihi, domain_ilo, domain_ihi);
 
-            if (extdir_ilo and i == domain_ilo) {
-                umns = q(i-1,j,k,n);
-                upls = q(i-1,j,k,n);
-            } else if (extdir_ihi and i == domain_ihi+1) {
-                umns = q(i,j,k,n);
-                upls = q(i,j,k,n);
-            }
-
             Ipx(i-1,j,k,n) = umns;
             Imx(i  ,j,k,n) = upls;
         });
@@ -129,14 +121,6 @@ void godunov::predict_plm_y (Box const& bx_in, int ncomp,
             Real vmns = q(i,j-1,k,n) + 0.5 * ( 1.0 - vcc(i,j-1,k,1) * dtdy) *
                 incflo_ho_yslope_extdir(i,j-1,k,n,q, extdir_jlo, extdir_jhi, domain_jlo, domain_jhi);
 
-            if (extdir_jlo and j == domain_jlo) {
-                vmns = q(i,j-1,k,n);
-                vpls = q(i,j-1,k,n);
-            } else if (extdir_jhi and j == domain_jhi+1) {
-                vmns = q(i,j,k,n);
-                vpls = q(i,j,k,n);
-            }
-
             Ipy(i,j-1,k,n) = vmns;
             Imy(i,j  ,k,n) = vpls;
         });
@@ -197,14 +181,6 @@ void godunov::predict_plm_z (Box const& bx_in, int ncomp,
                 incflo_ho_zslope_extdir(i,j,k,n,q, extdir_klo, extdir_khi, domain_klo, domain_khi);
             Real wmns = q(i,j,k-1,n) + 0.5 * ( 1.0 - vcc(i,j,k-1,2) * dtdz) *
                 incflo_ho_zslope_extdir(i,j,k-1,n,q, extdir_klo, extdir_khi, domain_klo, domain_khi);
-
-            if (extdir_klo and k == domain_klo) {
-                wmns = q(i,j,k-1,n);
-                wpls = q(i,j,k-1,n);
-            } else if (extdir_khi and k == domain_khi+1) {
-                wmns = q(i,j,k,n);
-                wpls = q(i,j,k,n);
-            }
 
             Ipz(i,j,k-1,n) = wmns;
             Imz(i,j,k  ,n) = wpls;
