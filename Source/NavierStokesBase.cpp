935ef547a664cfecd1c4c49516896b2d7a5df814
Michele Rosso <mrosso@lbl.gov>
2020-07-23 16:46:48 -0700

    Godunov: pass switches from Godunov class to new implementation.

--- Source/NavierStokesBase.cpp
+++ Source/NavierStokesBase.cpp
@@ -3749,7 +3749,7 @@ NavierStokesBase::velocity_advection (Real dt)
                                                        forcing_term.array(U_mfi),
                                                        geom, dt, &math_bcs[0],
                                                        iconserv.data(),
-                                                       tmpfab.dataPtr(), true, true, false);
+                                                       tmpfab.dataPtr(), (Godunov::ppm_type > 0), true, Godunov::use_forces_in_trans);
 
                     Gpu::streamSynchronize();
 
