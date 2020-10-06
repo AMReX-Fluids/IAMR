2a14225bd1735bf7d1e6227c4494a53fabfda8b8
cgilet <cgilet@gmail.com>
2020-10-05 22:57:09 -0700

    Another typo

--- Source/NavierStokes.cpp
+++ Source/NavierStokes.cpp
@@ -657,7 +657,7 @@ NavierStokes::scalar_advection (Real dt,
                          D_DECL(u_mac[0],u_mac[1],u_mac[2]),
                          D_DECL(edgstate[0],edgstate[1],edgstate[2]), 0, false,
                          D_DECL(cfluxes[0],cfluxes[1],cfluxes[2]), 0,
-                         m_bcrec_scalars, m_bcrec_scalars.dataPtr(), geom  );
+                         m_bcrec_scalars, m_bcrec_scalars_d.dataPtr(), geom  );
 
         if (do_reflux)
         {
