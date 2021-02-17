175037d7c6399debb9a00f1f94810c23dd9cc114
cgilet <cgilet@gmail.com>
2021-02-17 12:48:12 -0500

    WIP: more problem setups in c++

--- Source/prob/prob_init.cpp
+++ Source/prob/prob_init.cpp
@@ -3,15 +3,19 @@
 
 using namespace amrex;
  
-
-bool NavierStokes::prob_initialized = false;
 int NavierStokes::probtype = -1;
-Real NavierStokes::adv_vel = 0.;
-int NavierStokes::adv_dir = 0;
-Real NavierStokes::denfact = 1.0;
 
-Vector<Real> NavierStokes::blob(AMREX_SPACEDIM, 0.);
-Real NavierStokes::radblob = 0.1;
+//
+// Initial Conditions parameters
+//
+namespace IC
+{
+  Real density_ic = 1.0;
+  GpuArray<Real, AMREX_SPACEDIM> velocity_ic {AMREX_D_DECL( 0., 0., 0. )};
+  
+  Real radblob = 0.1;
+  GpuArray<Real, AMREX_SPACEDIM> blob {AMREX_D_DECL( 0., 0., 0. )};
+}
 
 //
 // Initialize state and pressure with problem-specific data
@@ -21,27 +25,35 @@ void NavierStokes::prob_initData ()
     //
     // Read problem parameters from inputs file
     //
-    if ( !prob_initialized )
     {
       ParmParse pp("prob");
-
-      pp.query("probtype",probtype);
-      pp.query("adv_vel",adv_vel);
-      pp.query("adv_dir",adv_dir);
-      pp.query("denfact",denfact);
       
-      pp.queryarr("blob_center",blob,0,AMREX_SPACEDIM);
-      pp.query("blob_radius",radblob);
+      pp.query("probtype",probtype);
+      pp.query("density_ic",IC::density_ic);
 
-      prob_initialized = true;
+      Vector<Real> vel(AMREX_SPACEDIM, 0.);
+      pp.queryarr("velocity_ic",vel,0,AMREX_SPACEDIM);
+      if (!vel.empty())
+      {
+	IC::velocity_ic = {AMREX_D_DECL(vel[0], vel[1], vel[2])};
+      }
+      
+      pp.query("blob_radius",IC::radblob);
+      Vector<Real> cen(AMREX_SPACEDIM, 0.);
+      pp.queryarr("blob_center",cen,0,AMREX_SPACEDIM);
+      if (!cen.empty())
+      {
+	IC::blob = {AMREX_D_DECL(cen[0], cen[1], cen[2])};
+      }
     }
 
     //
     // Fill state and, optionally, pressure
     //
-    MultiFab& S_new = get_new_data(State_Type);
     MultiFab& P_new = get_new_data(Press_Type);
-
+    MultiFab& S_new = get_new_data(State_Type);
+    const int nscal = NUM_STATE-Density;
+    
     S_new.setVal(0.0);
     P_new.setVal(0.0);
 
@@ -57,18 +69,24 @@ void NavierStokes::prob_initData ()
     {
         const Box& vbx = mfi.tilebox();
 
-	if (1 == probtype )
+	if ( 1 == probtype )
         {
 	  //
 	  // Start from rest, constant density of 1
 	  //
 	  S_new[mfi].setVal<RunOn::Gpu>(1.0,Density);
 	}
-        else if (2 == probtype || 6 == probtype )
+        else if ( 2 == probtype || 6 == probtype )
 	{
-	  init_bubble(vbx, P_new.array(mfi),
-		      S_new.array(mfi, Xvel), S_new.array(mfi, Density),
+	  init_bubble(vbx, P_new.array(mfi), S_new.array(mfi, Xvel),
+		      S_new.array(mfi, Density), nscal,
 		      domain, dx, problo, probhi);
+	}
+        else if ( 4 == probtype )
+	{
+	  init_channel(vbx, P_new.array(mfi), S_new.array(mfi, Xvel),
+		       S_new.array(mfi, Density), nscal,
+		       domain, dx, problo, probhi);
 	}
 	else
         {
@@ -81,40 +99,16 @@ void NavierStokes::init_bubble (Box const& vbx,
 				Array4<Real> const& press,
 				Array4<Real> const& vel,
 				Array4<Real> const& scal,
+				const int nscal,
 				Box const& domain,
 				GpuArray<Real, AMREX_SPACEDIM> const& dx,
 				GpuArray<Real, AMREX_SPACEDIM> const& problo,
 				GpuArray<Real, AMREX_SPACEDIM> const& probhi)
 {
-  //
-  // set up flow direction
-  //
-  Real xvel=0.0, yvel=0.0, zvel=0.0;
-
-  if ( adv_dir == 0 )
-  {
-    xvel = adv_vel;
-  }
-  else if ( adv_dir == 1 )
-  {
-    yvel = adv_vel;
-  }
-#if (AMREX_SPACEDIM == 3)
-  else if ( adv_dir == 2 )
-  {
-    zvel = adv_vel;
-  }
-#endif
-  else
-  {
-    Abort("init_bubble: unknown adv_dir");
-  }
-
   const auto domlo = amrex::lbound(domain);
   //
   // FIXME - don't think static vars get captured on GPU, so have to make local copies
   //
-  const int ntrac = do_trac2 ? 2 : 1;
   bool rise = probtype==6; //HotSpot problem
 
   amrex::ParallelFor(vbx, [=] AMREX_GPU_DEVICE (int i, int j, int k) noexcept
@@ -125,18 +119,19 @@ void NavierStokes::init_bubble (Box const& vbx,
     //
     // Fill Velocity
     //
-    vel(i,j,k,0) = xvel;
-    vel(i,j,k,1) = yvel;
+    vel(i,j,k,0) = IC::velocity_ic[0];
+    vel(i,j,k,1) = IC::velocity_ic[1];
     
 #if (AMREX_SPACEDIM == 3)
     Real z = problo[2] + (k - domlo.z + 0.5)*dx[2];
 
-    vel(i,j,k,2) = zvel;
+    vel(i,j,k,2) = IC::velocity_ic[2];
 #endif
 
-    Real dist = std::sqrt((x-blob[0])*(x-blob[0]) + (y-blob[1])*(y-blob[1])
+    Real dist = std::sqrt( (x-IC::blob[0])*(x-IC::blob[0])
+			  + (y-IC::blob[1])*(y-IC::blob[1])
 #if (AMREX_SPACEDIM == 3)
-			  + (z-blob[2])*(z-blob[2])
+			  + (z-IC::blob[2])*(z-IC::blob[2])
 #endif
 			  );
     //
@@ -144,8 +139,8 @@ void NavierStokes::init_bubble (Box const& vbx,
     //
 
     // Tracers
-    scal(i,j,k,1) = dist < radblob ? 1.0 : 0.0;
-    for ( int nt=2; nt<ntrac; nt++)
+    scal(i,j,k,1) = dist < IC::radblob ? 1.0 : 0.0;
+    for ( int nt=2; nt<nscal; nt++)
     {
       scal(i,j,k,nt) = 1.0;
     }
@@ -153,16 +148,65 @@ void NavierStokes::init_bubble (Box const& vbx,
     if ( rise )
     {
       // Density for Hot/less dense bubble rising
-      scal(i,j,k,0) = 1.0/denfact + 0.5*(1.0 - 1.0/denfact) 
-	                               *(1.0 + std::tanh(40.*(dist - radblob)));
+      scal(i,j,k,0) = 1.0/IC::density_ic + 0.5*(1.0 - 1.0/IC::density_ic) 
+	                               *(1.0 + std::tanh(40.*(dist - IC::radblob)));
       //Temp
-      scal(i,j,k,ntrac+1) = 1/scal(i,j,k,0);
+      scal(i,j,k,nscal-1) = 1/scal(i,j,k,0);
     }
     else
     {
       // Density for dense bubble falling. 
-      scal(i,j,k,0) = 1.0 + 0.5*(denfact-1.0)*(1.0-std::tanh(30.*(dist-radblob)));
+      scal(i,j,k,0) = 1.0 + 0.5*(IC::density_ic-1.0)*(1.0-std::tanh(30.*(dist-IC::radblob)));
     }
     
+  });
+}
+
+void NavierStokes::init_channel (Box const& vbx,
+				 Array4<Real> const& press,
+				 Array4<Real> const& vel,
+				 Array4<Real> const& scal,
+				 const int nscal,
+				 Box const& domain,
+				 GpuArray<Real, AMREX_SPACEDIM> const& dx,
+				 GpuArray<Real, AMREX_SPACEDIM> const& problo,
+				 GpuArray<Real, AMREX_SPACEDIM> const& probhi)
+{  
+  const auto domlo = amrex::lbound(domain);
+
+  amrex::ParallelFor(vbx, [=] AMREX_GPU_DEVICE (int i, int j, int k) noexcept
+  {
+    Real x = problo[0] + (i - domlo.x + 0.5)*dx[0];
+    Real y = problo[1] + (j - domlo.y + 0.5)*dx[1];
+
+    //
+    // Fill Velocity
+    //
+    vel(i,j,k,0) = IC::velocity_ic[0];
+    vel(i,j,k,1) = IC::velocity_ic[1];
+    
+#if (AMREX_SPACEDIM == 3)
+    Real z = problo[2] + (k - domlo.z + 0.5)*dx[2];
+
+    vel(i,j,k,2) = IC::velocity_ic[2];
+#endif
+
+    Real dist = std::sqrt( (x-IC::blob[0])*(x-IC::blob[0])
+			  + (y-IC::blob[1])*(y-IC::blob[1])
+#if (AMREX_SPACEDIM == 3)
+			  + (z-IC::blob[2])*(z-IC::blob[2])
+#endif
+			  );
+    //
+    // Scalars, ordered as Density, Tracer(s), Temp (if using)
+    //
+    scal(i,j,k,0) = IC::density_ic;
+      
+    // Tracers
+    scal(i,j,k,1) = dist < IC::radblob ? 1.0 : 0.0;
+    for ( int nt=2; nt<nscal; nt++)
+    {
+      scal(i,j,k,nt) = 1.0;
+    }
   });
 }
