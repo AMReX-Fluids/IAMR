3ec154b7997ba366e4c465f30bc698a39df38ce3
cgilet <cgilet@gmail.com>
2021-08-18 22:02:07 -0400

    Remove unused options in Projection.cpp

--- Source/Projection.cpp
+++ Source/Projection.cpp
@@ -43,8 +43,6 @@ Real Projection::proj_abs_tol        = 1.e-16;
 int  Projection::add_vort_proj       = 0;
 int  Projection::do_outflow_bcs      = 1;
 int  Projection::rho_wgt_vel_proj    = 0;
-int  Projection::make_sync_solvable  = 0;
-Real Projection::divu_minus_s_factor = 0.0;
 
 namespace
 {
@@ -78,8 +76,6 @@ Projection::Initialize ()
     pp.query("add_vort_proj",       add_vort_proj);
     pp.query("do_outflow_bcs",      do_outflow_bcs);
     pp.query("rho_wgt_vel_proj",    rho_wgt_vel_proj);
-    pp.query("divu_minus_s_factor", divu_minus_s_factor);
-    pp.query("make_sync_solvable",  make_sync_solvable);
 
     pp.query("agglomeration",       agglomeration);
     pp.query("consolidation",       consolidation);
