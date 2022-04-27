c0c834eefe1d9b731b41503719252eb1b6fbde96
cgilet <cgilet@gmail.com>
2022-04-27 14:07:21 -0400

    Update docs: some projections info moved to AMReX-Hydro docs, ref that instead of repeating.

--- Docs/sphinx_documentation/source/conf.py
+++ Docs/sphinx_documentation/source/conf.py
@@ -56,7 +56,8 @@ bibtex_bibfiles = ["refs.bib"]
 #     #                    '../../sphinx_tutorials/build/html/objects.inv')
 # }
 intersphinx_mapping = {
-     'amrex': ('https://amrex-codes.github.io/amrex/docs_html/', None)
+     'amrex': ('https://amrex-codes.github.io/amrex/docs_html/', None),
+     'hydro': ('https://amrex-codes.github.io/amrex/hydro_html/', None)
 }
 
 # Add any paths that contain templates here, relative to this directory.
