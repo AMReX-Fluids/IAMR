22563e42562d781ed1830a19a702f014da31958d
cgilet <cgilet@users.noreply.github.com>
2023-02-14 18:44:14 +0000

    Deploying to gh-pages from  @ ef7039066fbdb5df4f3ffb43a8be750e4aad8fec 🚀

--- docs_html/_static/documentation_options.js
+++ docs_html/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '22.01-dev',
+    VERSION: '23.03-dev',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
